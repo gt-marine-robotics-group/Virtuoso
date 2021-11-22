305d9bebfba46f6f786346a0c42355e5fe96d59b
Manuel Roglan <manueljoseph113@gmail.com>
2021-11-21 18:54:34 -0800

    get all launch files automatically

--- virtuoso_processing/setup.py
+++ virtuoso_processing/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'virtuoso_processing'
 
@@ -10,9 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/'+ package_name, ['launch/lidar_processing.launch.py']),
-        ('share/' + package_name, ['launch/camera_processing.launch.py']),
-        ('share/' + package_name, ['launch/main.launch.py'])
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
