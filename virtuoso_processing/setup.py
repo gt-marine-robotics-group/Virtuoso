9c4c439dc9e4911b4b11a6c6d55e0cc4bd68d82e
Manuel Roglan <manueljoseph113@gmail.com>
2021-12-07 12:18:29 -0800

    added param files to data_files

--- virtuoso_processing/setup.py
+++ virtuoso_processing/setup.py
@@ -11,7 +11,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'param'), glob(os.path.join('param', '*.param.yaml'))),
+        (os.path.join('share', package_name), ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py')))
     ],
     install_requires=['setuptools'],
