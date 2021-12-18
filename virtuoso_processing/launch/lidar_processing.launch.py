291024dc0122fde2680b224100aa353ccb91c054
Manuel Roglan <59785089+mroglan@users.noreply.github.com>
2021-12-18 15:35:47 -0500

    Adding STVL for downsampling (#11)
    Added STVL for downsampling of pointclouds along with corresponding documentation

--- virtuoso_processing/launch/lidar_processing.launch.py
+++ virtuoso_processing/launch/lidar_processing.launch.py
@@ -2,12 +2,23 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
 
-# Using ray_ground_classifier and voxel_grid_node from Autoware.auto
+# Using ray_ground_classifier from Autoware.auto for ground filter
+# Using STVL to downsample and generate Costmap
 
 def generate_launch_description(): 
 
-    ray_ground_classifier_param_file = 'virtuoso_processing/param/ray_ground_classifier.param.yaml'
+    pkg_share = get_package_share_directory('virtuoso_processing')
+
+    # the launch file which we pass stvl in as a plugin an the params file
+    bringup_launch_file = os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'navigation_launch.py')
+    nav2_params_file = os.path.join(pkg_share, 'param', 'stvl.param.yaml')
+
+    ray_ground_classifier_param_file = os.path.join(pkg_share, 'param/ray_ground_classifier.param.yaml')
 
     ray_ground_classifier_param = DeclareLaunchArgument(
         'ray_ground_classifier_param_file',
@@ -15,31 +26,34 @@ def generate_launch_description():
         description='Path to config file for Ray Ground Classifier'
     )
 
-    voxel_grid_node_param_file = 'virtuoso_processing/param/voxel_grid_node.param.yaml'
-
-    voxel_grid_node_param = DeclareLaunchArgument(
-        'voxel_grid_node_param_file',
-        default_value=voxel_grid_node_param_file,
-        description='Path to config file for Voxel Grid Node'
-    )
-
-
     return LaunchDescription([
+
+        # running ground filter on raw LIDAR data
         ray_ground_classifier_param,
-        voxel_grid_node_param,
         Node(
             package='ray_ground_classifier_nodes',
             executable='ray_ground_classifier_cloud_node_exe',
             parameters=[LaunchConfiguration('ray_ground_classifier_param_file')],
             remappings=[("points_in", "/horizontal_laser_3d")] # points_in comes from raw LIDAR data
         ),
+
+        # downsampling LIDAR data with STVL (also generates a costmap)
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(bringup_launch_file),launch_arguments={'params_file': nav2_params_file}.items()),
+        Node(package='nav2_map_server', executable='map_server', name='map_server', output='screen', arguments=[nav2_params_file]),
+        # Rosbag used to test STVL with has a frame of horizontal_vlp16_link, so we need to 
+        # convert from that frame to base_link and from base_link to map using tf2_ros.
+        # For now, assuming no transformation needed between frames (hence all the 0s),
+        # but will likely change in future.
         Node(
-            package='voxel_grid_nodes',
-            executable='voxel_grid_node_exe',
-            parameters=[LaunchConfiguration('voxel_grid_node_param_file')],
-            remappings=[
-                ("points_in", "points_nonground"), # points_in come from topic published to by ray_ground_classifier_nodes 
-                ("points_downsampled", "points_fused_downsampled")
-            ]
-        ) 
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='base_link_to_map',
+        arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'map'],
+        ),
+        Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='horizontal_to_base_link',
+        arguments=['0', '0', '0', '0', '0', '0', 'horizontal_vlp16_link', 'base_link'],
+        )
     ])
--- virtuoso_processing/launch/main.launch.py
+++ virtuoso_processing/launch/main.launch.py
@@ -1,10 +1,14 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
 
+    pkg_share = get_package_share_directory('virtuoso_processing')
+
     return LaunchDescription([
-        IncludeLaunchDescription(PythonLaunchDescriptionSource('virtuoso_processing/launch/camera_processing.launch.py')),
-        IncludeLaunchDescription(PythonLaunchDescriptionSource('virtuoso_processing/launch/lidar_processing.launch.py'))
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/camera_processing.launch.py'))),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/lidar_processing.launch.py')))
     ])
