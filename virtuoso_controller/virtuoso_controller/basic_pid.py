2cf63710a57f1cbae088ba1662df2a667d1f2a50
Douglas Chin <douglassetochin@gmail.com>
2022-01-20 11:12:39 -0800

    added basic translational station keeping

--- virtuoso_controller/virtuoso_controller/basic_pid.py
+++ virtuoso_controller/virtuoso_controller/basic_pid.py
@@ -6,9 +6,12 @@ from geometry_msgs.msg import Vector3Stamped
 from geometry_msgs.msg import Quaternion
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Float32
+import tf_transformations
+
+
 
 #import pyproj
-#import math
+import numpy
 
 
 class basicPID(Node):
@@ -22,7 +25,7 @@ class basicPID(Node):
         self.leftFrontPubAngle = self.create_publisher(Float32, '/wamv/thrusters/left_front_thrust_angle', 10)
         self.rightFrontPubAngle = self.create_publisher(Float32, '/wamv/thrusters/right_front_thrust_angle', 10)
         self.leftRearPubAngle = self.create_publisher(Float32, '/wamv/thrusters/left_rear_thrust_angle', 10)
-        self.rightRearPubAngle = self.create_publisher(Float32, '/wamv/thrusters/left_rear_thrust_angle', 10)
+        self.rightRearPubAngle = self.create_publisher(Float32, '/wamv/thrusters/right_rear_thrust_angle', 10)
 
         self.leftFrontPubCmd = self.create_publisher(Float32, '/wamv/thrusters/left_front_thrust_cmd', 10)
         self.rightFrontPubCmd = self.create_publisher(Float32, '/wamv/thrusters/right_front_thrust_cmd', 10)             
@@ -44,18 +47,109 @@ class basicPID(Node):
             10)         
         self.odom_subscriber
         self.waypoint_subscriber
+        
+
 
     def odometry_callback(self, msg):
         self.stateEstimate = msg
     
     def waypoint_callback(self, msg):
         self.targetWaypoint = msg        
-    	self.run_pid()
+        self.run_pid()
 
     def run_pid(self):
-    	targetX = self.targetWaypoint.pose.pose.position.x
-    	targetY = self.targetWaypoint.pose.pose.position.y    	
+        targetX = self.targetWaypoint.pose.pose.position.x
+        targetY = self.targetWaypoint.pose.pose.position.y    
         
+        currentVelX = self.stateEstimate.twist.twist.linear.x
+        currentVelY = self.stateEstimate.twist.twist.linear.y
+    	
+        currentX = self.stateEstimate.pose.pose.position.x
+        currentY = self.stateEstimate.pose.pose.position.y
+    	
+        velocityX = targetX - currentX
+        velocityY = targetY - currentY
+        if numpy.sqrt(velocityX**2 + velocityY**2) >= 4.0:
+             velocityX = velocityX/numpy.sqrt(velocityX**2 + velocityY**2)*4.0
+             velocityY = velocityY/numpy.sqrt(velocityX**2 + velocityY**2)*4.0
+        targetVel = [velocityX, velocityY, 0.0, 0.0]
+        #self.get_logger().info('targetVel: ' + str(targetVel)) 
+        q = [self.stateEstimate.pose.pose.orientation.x, self.stateEstimate.pose.pose.orientation.y, self.stateEstimate.pose.pose.orientation.z, self.stateEstimate.pose.pose.orientation.w]
+
+    	
+        q_inv = q.copy()
+        q_inv[0] = -q_inv[0]
+        q_inv[1] = -q_inv[1]
+        q_inv[2] = -q_inv[2]
+
+        targetVel = tf_transformations.quaternion_multiply(q_inv, targetVel)
+        targetVel = tf_transformations.quaternion_multiply(targetVel, q)
+        
+        targetForceY = targetVel[1] - currentVelY
+        targetForceX = targetVel[0] - currentVelX
+        targetForceMag = numpy.sqrt(targetForceY**2 + targetForceX**2)
+        #self.get_logger().info('targetVel: ' + str(targetVel))   	
+        theta_targetForce = numpy.arctan2(targetForceY, targetForceX)
+
+        #self.get_logger().info('vehicle: ' + str(theta_vehicle))
+        self.get_logger().info('theta_targetForce: ' + str(theta_targetForce*180/numpy.pi))   	
+        #self.get_logger().info('targetForceMag: ' + str(targetForceY))
+        targetForce = Float32()
+        targetForce.data = targetForceMag/10.0
+        #self.get_logger().info('targetForce: ' + str(targetForce))  
+        msg1 = Float32()
+        msg1.data = (45*numpy.pi/180+theta_targetForce)
+        msg2 = Float32()
+        msg2.data = -(45*numpy.pi/180-theta_targetForce)
+        
+        if theta_targetForce >=-135*numpy.pi/180 and theta_targetForce <=45*numpy.pi/180:
+             self.leftFrontPubAngle.publish(msg1)
+             self.rightRearPubAngle.publish(msg1)
+             self.leftFrontPubCmd.publish(targetForce)
+             self.rightRearPubCmd.publish(targetForce)
+             #self.get_logger().info('Mode: Forward Right')  
+        if theta_targetForce >=-45*numpy.pi/180 and theta_targetForce <=135*numpy.pi/180:
+             self.rightFrontPubAngle.publish(msg2)
+             self.leftRearPubAngle.publish(msg2)
+             self.rightFrontPubCmd.publish(targetForce)
+             self.leftRearPubCmd.publish(targetForce)
+             #self.get_logger().info('Mode: Forward Left')  
+        if theta_targetForce >-180*numpy.pi/180 and theta_targetForce <-135*numpy.pi/180:
+             msg2.data = (90*numpy.pi/180)
+             self.rightFrontPubAngle.publish(msg2)
+             self.leftRearPubAngle.publish(msg2)
+             self.rightFrontPubCmd.publish(targetForce)
+             self.leftRearPubCmd.publish(targetForce)
+             
+             phi = theta_targetForce+180.0 + 45.0 
+             phi = theta_targetForce+180.0 + phi              
+             
+             msg1.data = -(180 - 45 - phi)
+             
+             self.leftFrontPubAngle.publish(msg1)
+             self.rightRearPubAngle.publish(msg1)
+             self.leftFrontPubCmd.publish(targetForce)
+             self.rightRearPubCmd.publish(targetForce)   
+             self.get_logger().info('Mode: Back 1')  
+        if theta_targetForce > 135*numpy.pi/180 and theta_targetForce < 180*numpy.pi/180:
+             phi = -(theta_targetForce-180.0) + 45.0 
+             phi = theta_targetForce-180.0 - phi     
+             msg2.data = (180 - 45 - phi)
+             self.rightFrontPubAngle.publish(msg2)
+             self.leftRearPubAngle.publish(msg2)
+             self.rightFrontPubCmd.publish(targetForce)
+             self.leftRearPubCmd.publish(targetForce)
+             
+             phi = theta_targetForce+180.0 + 45.0 
+             phi = theta_targetForce+180.0 + phi              
+             
+             msg1.data = -(90*numpy.pi/180)#(180 - 45 - phi)
+             
+             self.leftFrontPubAngle.publish(msg1)
+             self.rightRearPubAngle.publish(msg1)
+             self.leftFrontPubCmd.publish(targetForce)
+             self.rightRearPubCmd.publish(targetForce)  
+             self.get_logger().info('Mode: Back 2')  
 def main(args=None):
     rclpy.init(args=args)
 
