9748306807497652842a746b418d3bacea467974
mroglan <manueljoseph113@gmail.com>
2022-01-28 13:15:11 -0500

    added launch file and entry_point

--- virtuoso_navigation/setup.py
+++ virtuoso_navigation/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'virtuoso_navigation'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'get_points = virtuoso_navigation.get_points:main'
         ],
     },
 )
