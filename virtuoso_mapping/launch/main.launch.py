fd67ad08d1696bdc50628c67f2d07b6a4da9a126
mroglan <manueljoseph113@gmail.com>
2022-01-28 13:14:46 -0500

    added astar to launch

--- virtuoso_mapping/launch/main.launch.py
+++ virtuoso_mapping/launch/main.launch.py
@@ -8,8 +8,10 @@ def generate_launch_description():
 
     processing_pkg = get_package_share_directory('virtuoso_processing')
     localization_pkg = get_package_share_directory('virtuoso_localization')
+    navigation_pkg = get_package_share_directory('virtuoso_navigation')
 
     return LaunchDescription([
         IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(processing_pkg, 'launch/main.launch.py'))),
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(localization_pkg, 'launch/ekf.launch.py')))
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(localization_pkg, 'launch/ekf.launch.py'))),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(navigation_pkg, 'launch/astar.launch.py')))
     ])
