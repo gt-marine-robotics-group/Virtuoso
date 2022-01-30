449463d6078e7404ff97f08e5768f65d541191d8
mroglan <manueljoseph113@gmail.com>
2022-01-30 17:20:38 -0500

    using custom messages

--- virtuoso_navigation/virtuoso_navigation/get_points.py
+++ virtuoso_navigation/virtuoso_navigation/get_points.py
@@ -1,21 +1,22 @@
+# helpful resource: http://docs.ros.org/en/indigo/api/costmap_2d/html/costmap__2d_8h_source.html#l00171
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import OccupancyGrid
-from std_msgs.msg import Int8MultiArray
+from virtuoso_msgs.msg import CostmapPoints, CostmapPointInfo
+import math
 
 class GetPoints(Node):
 
     def __init__(self):
         super().__init__('get_points')
-        self.points_pub = self.create_publisher(Int8MultiArray, '/path_finding/points', 10)
+        self.points_pub = self.create_publisher(CostmapPoints, '/path_finding/points', 10)
         self.costmap_sub = self.create_subscription(OccupancyGrid, '/global_costmap/costmap', self.get_points, 10)
 
     def get_points(self, msg:OccupancyGrid):
 
-        points = []
+        points = CostmapPoints() 
 
         width = msg.info.width
-        height = msg.info.height
 
         origin_x = msg.info.origin.position.x
         origin_y = msg.info.origin.position.y
@@ -23,14 +24,24 @@ class GetPoints(Node):
         resolution = msg.info.resolution
 
         for i, cost in enumerate(msg.data):
+
+            point = CostmapPointInfo()
             
-            p_x = i - ((i / width) * width)
-            p_y = i / width
+            p_x = i - (math.trunc((i / width)) * width)
+            p_y = math.trunc(i / width)
 
             map_x = origin_x + (p_x * resolution)
             map_y = origin_y + (p_y * resolution)
             
-            points.append({'x': map_x, 'y': map_y, 'cost': cost})
+
+            point.x = map_x
+            point.y = map_y
+            point.cost = cost
+
+            points.data.append(point)
+        
+        points.width = width
+        points.height = msg.info.height
         
         self.points_pub.publish(points)
 
