31bc172f6a11c4ae760f8ce26894bdab56514adb
Manuel Roglan <59785089+mroglan@users.noreply.github.com>
2022-02-03 17:13:41 -0500

    Nav w nav2 (#15)
    * created lidar and camera pipelines
    
    * ignore install and log folders
    
    * added comments
    
    * get all launch files automatically
    
    * fixed exec depends
    
    * added autoware.auto packages
    
    * created vcs_repos w/ autoware and stvl
    
    * added documentation w/o images
    
    * added images
    
    * Added image for original LiDAR data visualization
    
    * added param files to data_files
    
    * added stvl params
    
    * using absolute paths
    
    * implementing stvl
    
    * added comments
    
    * removed voxel_grid_nodes
    
    * added script to spawn objects in gazebo
    
    * added a README
    
    * Update README.md
    
    * pulled main
    
    * changed stvl.param to nav2.param
    
    * launching processing and localization nodes
    
    * added processing and localization dependencies
    
    * added launch files
    
    * renamed to nav2.params
    
    * updated global_costmap params
    
    * updated frame_id
    
    * updated tf transforms
    
    * added pointcloud utils
    
    * added robot self filter
    
    * updated params
    
    * moved pointcloud utils to separate dir
    
    * removed unused params
    
    * dir for lidar processing
    
    * dir for camera processing
    
    * moved to new dir
    
    * updated entry points and packages
    
    * updated docs
    
    * increased size of local_costmap
    
    * updated global_costmap params
    
    * added test node for global_costmap
    
    * created README
    
    * updated README
    
    * added astar to launch
    
    * added launch file and entry_point
    
    * added get_points node
    
    * added astar launch file
    
    * removed unnecessary files
    
    * added planner and controller servers
    
    * added rviz to launch
    
    * removed entry_point
    
    * removed astar launch file
    
    * added navigation to launch
    
    * added set_goal to launch
    
    * added test for setting goal
    
    * added functionality for setting goal
    
    * added entry_points
    
    * added test for changing goal
    
    * attempted to implement changing goal
    
    * updated test
    
    * updated SetGoal
    
    * added documentation
    
    Co-authored-by: jehand <70664661+jehand@users.noreply.github.com>

--- virtuoso_navigation/setup.py
+++ virtuoso_navigation/setup.py
@@ -1,15 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+from glob import glob
 
 package_name = 'virtuoso_navigation'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +23,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'set_goal = virtuoso_navigation.set_goal:main',
+            'test_set_goal = virtuoso_navigation.testing.set_goal:main',
+            'test_change_goal = virtuoso_navigation.testing.change_goal:main'
         ],
     },
 )
