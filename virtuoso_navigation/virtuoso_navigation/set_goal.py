6172349081518446b81cd22ab13e0c1b0e1d798e
mroglan <manueljoseph113@gmail.com>
2022-02-02 22:17:44 -0500

    added functionality for setting goal

---
+++ virtuoso_navigation/virtuoso_navigation/set_goal.py
@@ -0,0 +1,61 @@
+import rclpy
+from rclpy.node import Node
+from nav2_msgs.action import NavigateToPose
+from geometry_msgs.msg import PoseStamped
+from rclpy.action import ActionClient
+
+class SetGoal(Node):
+
+    def __init__(self):
+        super().__init__('set_goal')
+        self.goal_sub = self.create_subscription(PoseStamped, '/virtuoso_navigation/set_goal', self.set_goal, 10)
+        self.nav_action = ActionClient(self, NavigateToPose, '/navigate_to_pose')
+
+    def set_goal(self, msg:PoseStamped):
+
+        self.get_logger().info('getting goal')
+        
+        self.nav_action.wait_for_server()
+
+        self.get_logger().info('done waiting for the server')
+
+        goal = NavigateToPose.Goal()
+        goal.pose = msg
+
+        self.future_goal = self.nav_action.send_goal_async(goal, feedback_callback=self.feedback_callback)
+
+        self.future_goal.add_done_callback(self.goal_response_callback)
+
+    
+    def feedback_callback(self, msg):
+        self.get_logger().info(str(msg))
+    
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected')
+            return
+        
+        self.get_logger().info('Goal accepted')
+
+        self.get_result_future = goal_handle.get_result_async()
+        self.get_result_future.add_done_callback(self.goal_done_callback)
+
+    def goal_done_callback(self, future):
+        result = future.result()
+        self.get_logger().info('Receieved result: ' + str(result))
+
+
+def main(args=None):
+    
+    rclpy.init(args=args)
+
+    node = SetGoal()
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
