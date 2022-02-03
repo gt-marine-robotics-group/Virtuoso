2b01890380f2f9ea66d2df271f92875477522955
mroglan <manueljoseph113@gmail.com>
2022-02-02 23:27:47 -0500

    attempted to implement changing goal

--- virtuoso_navigation/virtuoso_navigation/set_goal.py
+++ virtuoso_navigation/virtuoso_navigation/set_goal.py
@@ -1,4 +1,5 @@
 import rclpy
+from rclpy.task import Future
 from rclpy.node import Node
 from nav2_msgs.action import NavigateToPose
 from geometry_msgs.msg import PoseStamped
@@ -10,6 +11,7 @@ class SetGoal(Node):
         super().__init__('set_goal')
         self.goal_sub = self.create_subscription(PoseStamped, '/virtuoso_navigation/set_goal', self.set_goal, 10)
         self.nav_action = ActionClient(self, NavigateToPose, '/navigate_to_pose')
+        self.goal_accepted = False
 
     def set_goal(self, msg:PoseStamped):
 
@@ -17,31 +19,46 @@ class SetGoal(Node):
         
         self.nav_action.wait_for_server()
 
-        self.get_logger().info('done waiting for the server')
+        if not self.goal_accepted:
+            self.set_new_goal(msg)
+            return
+        
+        self.change_goal(msg)
 
-        goal = NavigateToPose.Goal()
-        goal.pose = msg
+    def change_goal(self, msg):
+        self.get_logger().info('cancelling goal')
+        self.cancel = self.nav_action._cancel_goal_async(self.future_goal.result())
+        self.cancel.add_done_callback(self.set_new_goal(msg))
 
-        self.future_goal = self.nav_action.send_goal_async(goal, feedback_callback=self.feedback_callback)
+    def set_new_goal(self, msg:PoseStamped):
+        self.goal = NavigateToPose.Goal()
+        self.goal.pose = msg
+
+        self.get_logger().info('setting goal')
+
+        self.future_goal = self.nav_action.send_goal_async(self.goal, feedback_callback=self.feedback_callback)
 
         self.future_goal.add_done_callback(self.goal_response_callback)
 
     
     def feedback_callback(self, msg):
-        self.get_logger().info(str(msg))
+        # self.get_logger().info(str(msg))
+        pass
     
-    def goal_response_callback(self, future):
+    def goal_response_callback(self, future:Future):
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info('Goal rejected')
             return
         
+        self.goal_accepted = True
         self.get_logger().info('Goal accepted')
 
         self.get_result_future = goal_handle.get_result_async()
         self.get_result_future.add_done_callback(self.goal_done_callback)
 
-    def goal_done_callback(self, future):
+    def goal_done_callback(self, future:Future):
+        self.goal_accepted = False
         result = future.result()
         self.get_logger().info('Receieved result: ' + str(result))
 
