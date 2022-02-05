034ed3ba4702ce229c55aefc851809a6d20b1169
mroglan <manueljoseph113@gmail.com>
2022-02-05 13:53:17 -0500

    added euclidean clustering

--- virtuoso_perception/setup.py
+++ virtuoso_perception/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'virtuoso_perception'
 
@@ -9,7 +11,9 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'param'), glob(os.path.join('param', '*.param.yaml'))),
+        (os.path.join('share', package_name), ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
