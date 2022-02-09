3ac994829a4d99385c26581a8ce43106a0ba7027
mroglan <manueljoseph113@gmail.com>
2022-02-08 20:51:32 -0500

    added find_buoys

--- virtuoso_perception/setup.py
+++ virtuoso_perception/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 from glob import glob
 
@@ -7,7 +7,7 @@ package_name = 'virtuoso_perception'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -24,6 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'find_buoys = virtuoso_perception.buoys.find_buoys:main'
         ],
     },
 )
