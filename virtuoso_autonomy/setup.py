efe0f9df5289506e9907cad94bab03ce6ca758d8
mroglan <manueljoseph113@gmail.com>
2022-02-08 20:50:08 -0500

    added task3 launch

--- virtuoso_autonomy/setup.py
+++ virtuoso_autonomy/setup.py
@@ -1,15 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+from glob import glob
 
 package_name = 'virtuoso_autonomy'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
