26daa35cfbba91f57c68c5e2f22b5211e7905ab9
mroglan <manueljoseph113@gmail.com>
2022-02-10 23:13:56 -0500

    added classify_buoys to launch

--- virtuoso_perception/launch/find_and_classify_buoys.launch.py
+++ virtuoso_perception/launch/find_and_classify_buoys.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -11,5 +12,8 @@ def generate_launch_description():
     # must have virtuoso_processing lidar_processing launched as well for euclidean clustering to do anything
     return LaunchDescription([
         IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/find_buoys.launch.py'))),
-        # launch node for classifying buoys
+        Node(
+            package='virtuoso_perception',
+            executable='classify_buoys'
+        )        
     ])
