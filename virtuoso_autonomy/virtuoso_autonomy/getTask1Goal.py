c623c49fd05913c5f7838a907a346c951b4804db
Douglas Chin <douglassetochin@gmail.com>
2022-02-13 20:14:20 -0800

    initial work towards task1

---
+++ virtuoso_autonomy/virtuoso_autonomy/getTask1Goal.py
@@ -0,0 +1,76 @@
+import rclpy
+from rclpy.node import Node
+from geographic_msgs.msg import GeoPoseStamped
+from geometry_msgs.msg import PoseStamped
+from geodesy import utm
+import tf2_ros 
+#import tf2_geometry_msgs
+
+class getTask1Goal(Node):
+
+    def __init__(self):
+        super().__init__('get_task_1_goal')
+        self.receivedGoal = GeoPoseStamped()
+        self.goalToSend = PoseStamped()
+        
+        self.tf_buffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tf_buffer, self)
+
+        self.goalMapPublisher = self.create_publisher(PoseStamped, '/virtuoso_navigation/set_goal', 10)
+
+       
+        self.goal_subscriber = self.create_subscription(
+            GeoPoseStamped,
+            '/vrx/station_keeping/goal',
+            self.goal_callback,
+            10)          
+        self.goal_subscriber
+
+        
+    def goal_callback(self, msg):
+        #msg2 = Imu()
+        self.receivedGoal = msg
+        geopoint = msg.pose.position
+        lat = geopoint.latitude
+        lon = geopoint.longitude
+        
+        goalpoint = utm.fromLatLong(lat, lon)
+        
+        
+        self.goalToSend.header.frame_id = "utm"
+        #self.goalToSend.pose.position.x = goalpoint.easting
+        #self.goalToSend.pose.position.y = goalpoint.northing    
+        
+        self.goalToSend.pose.position.x = 5.0
+        self.goalToSend.pose.position.y = 5.0
+        
+        """
+        self.goalToSend.pose.orientation = msg.pose.orientation   
+        try:
+                transformation = self.tf_buffer.lookup_transform('map', 'utm', self.get_clock().now())
+                goalToSend = tf2_geometry_msgs.do_transform_pose(goalToSend, transformation)
+        except (tf2_ros.LookupException,
+                tf2_ros.ConnectivityException,
+                tf2_ros.ExtrapolationException):
+    	        pass
+    	        """
+        self.goalMapPublisher.publish(self.goalToSend)
+ 
+
+        
+def main(args=None):
+    rclpy.init(args=args)
+
+    get_task_1_goal = getTask1Goal()
+
+    rclpy.spin(get_task_1_goal)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    get_task_1_goal.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ virtuoso_autonomy/virtuoso_autonomy/test_waypoint_generator.py
@@ -0,0 +1,55 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Imu
+from sensor_msgs.msg import NavSatFix
+from geometry_msgs.msg import Vector3Stamped
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Quaternion
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float32
+from geographic_msgs.msg import GeoPoseStamped
+from geometry_msgs.msg import PoseStamped
+
+#import pyproj
+#import math
+import numpy
+
+class testWaypointGenerator(Node):
+
+    def __init__(self):
+        super().__init__('test_waypoint_generator')
+        
+
+        self.targetWaypoint = GeoPoseStamped()
+        self.targetWaypoint.pose.position.latitude = -33.722767309712644 + 0.0006
+        self.targetWaypoint.pose.position.longitude = 150.67399020302886 + 0.0006
+	
+        self.waypointPub = self.create_publisher(GeoPoseStamped, '/vrx/station_keeping/goal', 10)
+        timer_period = 0.05  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+
+    def timer_callback(self):
+        self.waypointPub.publish(self.targetWaypoint)
+
+       
+     
+    
+
+        
+def main(args=None):
+    rclpy.init(args=args)
+
+    test_waypoint_generator = testWaypointGenerator()
+
+    rclpy.spin(test_waypoint_generator)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    test_waypoint_generator.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
