4f55532a1c2319ea959989ae7729846019ab7dee
mroglan <manueljoseph113@gmail.com>
2022-02-14 23:25:49 -0500

    added entry point

--- virtuoso_autonomy/setup.py
+++ virtuoso_autonomy/setup.py
@@ -23,6 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'mission_interpreter = virtuoso_autonomy.mission_interpreter:main'
         ],
     },
 )
