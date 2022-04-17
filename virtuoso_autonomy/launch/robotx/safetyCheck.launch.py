6da78fa1daf350ebeb2e2d68650619d3fef6cfec
Manuel Roglan <manueljoseph113@gmail.com>
2022-04-17 18:45:30 -0400

    created robotx satefyCheck launch file

---
+++ virtuoso_autonomy/launch/robotx/safetyCheck.launch.py
@@ -0,0 +1,38 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    processing = get_package_share_directory('virtuoso_processing')
+    navigation = get_package_share_directory('virtuoso_navigation')
+    localization = get_package_share_directory('virtuoso_localization')
+    perception = get_package_share_directory('virtuoso_perception')
+    controller = get_package_share_directory('virtuoso_controller')
+
+    return LaunchDescription([
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(processing, 'launch', 'main.launch.py'))),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(localization, 'launch', 'ekf.launch.py'))),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(navigation, 'launch', 'main.launch.py'))),
+        # IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(perception, 'launch', 'find_and_classify_buoys.launch.py'))),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(controller, 'launch', 'main.launch.py'))),
+        Node(
+            package='virtuoso_autonomy',
+            executable='mission_interpreter'
+        ),
+        Node(
+            package='virtuoso_autonomy',
+            executable='station_keeping'
+        ),
+        Node(
+            package='virtuoso_autonomy',
+            executable='perception'
+        ),
+        Node(
+            package='virtuoso_autonomy',
+            executable='wayfinding'
+        )
+    ])
