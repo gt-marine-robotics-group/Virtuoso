6f0ff4b5161d07cc812687ad10d9dc9c29a84939
Douglas Chin <douglassetochin@gmail.com>
2022-09-05 18:11:40 -0400

    fixed motor positions

--- virtuoso_autonomy/setup.py
+++ virtuoso_autonomy/setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yml')))
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yml'))),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', 'robotx', '*.launch.py')))
     ],
     install_requires=['setuptools'],
@@ -30,7 +30,7 @@ setup(
             'station_keeping = virtuoso_autonomy.tasks.station_keeping.main:main',
             'wayfinding = virtuoso_autonomy.tasks.wayfinding.main:main',
             'f9p_gps_republish = virtuoso_autonomy.f9p_gps_republish:main',
-            'gx3_republish = virtuoso_autonomy.gx3_republish:main,
+            'gx3_republish = virtuoso_autonomy.gx3_republish:main',
             'robotX_safetyCheck = virtuoso_autonomy.robotx.safety_check.main:main',
             'robotX_gymkhana = virtuoso_autonomy.robotx.gymkhana.main:main'
         ],
