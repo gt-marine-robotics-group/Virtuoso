746127292d9be6b7f13772420324fd58194f87f2
Manuel Roglan <manueljoseph113@gmail.com>
2022-09-06 22:12:54 -0400

    moved sensor setup to new pkg

--- virtuoso_autonomy/setup.py
+++ virtuoso_autonomy/setup.py
@@ -13,7 +13,6 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yml'))),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', 'robotx', '*.launch.py')))
     ],
     install_requires=['setuptools'],
@@ -29,8 +28,6 @@ setup(
             'perception = virtuoso_autonomy.tasks.perception.main:main',
             'station_keeping = virtuoso_autonomy.tasks.station_keeping.main:main',
             'wayfinding = virtuoso_autonomy.tasks.wayfinding.main:main',
-            'f9p_gps_republish = virtuoso_autonomy.f9p_gps_republish:main',
-            'gx3_republish = virtuoso_autonomy.gx3_republish:main',
             'robotX_safetyCheck = virtuoso_autonomy.robotx.safety_check.main:main',
             'robotX_gymkhana = virtuoso_autonomy.robotx.gymkhana.main:main',
             'robotX_enter_exit = virtuoso_autonomy.robotx.enter_and_exit.main:main'
--- virtuoso_sensors/setup.py
+++ virtuoso_sensors/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'virtuoso_sensors'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yml')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +24,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'f9p_gps_republish = virtuoso_autonomy.f9p_gps_republish:main',
+            'gx3_republish = virtuoso_autonomy.gx3_republish:main',
         ],
     },
 )
