8a38bb27a6d689f39623431772376b8b0c588cce
George Burdell <gitmrobotics@gmail.com>
2022-09-13 11:20:38 -0400

    running autoware auto lidar

--- virtuoso_sensors/launch/lidar.launch.py
+++ virtuoso_sensors/launch/lidar.launch.py
@@ -1,41 +1,27 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import RegisterEventHandler, EmitEvent
-from launch.event_handlers import OnProcessExit
-from launch.events import Shutdown
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 import os
-import yaml
 
 def generate_launch_description():
 
     sensors = get_package_share_directory('virtuoso_sensors')
 
-    lidar_config = os.path.join(sensors, 'config', 'vlp16.yml')
-
-    pointcloud_converter_config = os.path.join(sensors, 'config', 'vlp16-convert.yml') 
-    with open(pointcloud_converter_config, 'r') as f:
-        pointcloud_converter_params = yaml.safe_load(f)['velodyne_convert_node']['ros_parameters']
-    pointcloud_converter_params['calibration'] = os.path.join(sensors, 'params', 'vlp16db.yml')
-
-    velodyne_node = Node(
-            package='velodyne_driver',
-            executable='velodyne_driver_node',
-            parameters=[lidar_config]
-        )
-
-    # pointcloud_converter_node = Node(
-    #     package='velodyne_pointcloud'
-    # )
+    config_file = os.path.join(sensors, 'config', 'vlp16.yml')
 
     return LaunchDescription([
-        velodyne_node,
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=velodyne_node,
-                on_exit=[EmitEvent(
-                    event=Shutdown()
-                )]
-            )
+        DeclareLaunchArgument(
+            'vlp16_node_param_file',
+            default_value=config_file
+        ),
+        Node(
+            package='velodyne_nodes',
+            namespace='lidar_front',
+            executable='velodyne_cloud_node_exe',
+            parameters=[LaunchConfiguration('vlp16_node_param_file')],
+            remappings=[("topic", "points_xyzi")],
+            arguments=["--model", "vlp16"]
         )
     ])
