9e043bd952c01a4dc49b6e94812e2f834ce699e4
Manuel Roglan <manueljoseph113@gmail.com>
2022-09-15 10:07:11 -0400

    added params to gymkhana launch

--- virtuoso_autonomy/launch/gymkhana.launch.py
+++ virtuoso_autonomy/launch/gymkhana.launch.py
@@ -1,11 +1,19 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
+
+    sim_time = DeclareLaunchArgument('sim_time', default_value='False')
+    sim_path = DeclareLaunchArgument('sim_path', default_value='')
+
+    sim_time_config = LaunchConfiguration('sim_time', default='False')
+    sim_path_config = LaunchConfiguration('sim_path', default='False')
 
     processing = get_package_share_directory('virtuoso_processing')
     navigation = get_package_share_directory('virtuoso_navigation')
@@ -14,9 +22,17 @@ def generate_launch_description():
     controller = get_package_share_directory('virtuoso_controller')
 
     return LaunchDescription([
+        sim_time,
+        sim_path,
         IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(processing, 'launch', 'main.launch.py'))),
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(localization, 'launch', 'ekf.launch.py'))),
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(navigation, 'launch', 'main.launch.py'))),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(localization, 'launch', 'ekf.launch.py')),
+            launch_arguments= {'sim_time': sim_time_config, 'sim_path': sim_path_config}.items()
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(navigation, 'launch', 'main.launch.py')),
+            launch_arguments={'sim_time': sim_time_config}.items()
+        ),
         IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(perception, 'launch', 'find_buoys.launch.py'))),
         IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(controller, 'launch', 'main.launch.py'))),
 
--- virtuoso_autonomy/launch/safetyCheck.launch.py
+++ virtuoso_autonomy/launch/safetyCheck.launch.py
@@ -1,8 +1,8 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument, GroupAction
-from launch_ros.actions import Node, PushRosNamespace
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
