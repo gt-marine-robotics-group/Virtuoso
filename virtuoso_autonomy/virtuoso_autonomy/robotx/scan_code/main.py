eda3637ba9b84ca0d6b25ef2fd6ccde916e3c47b
Manuel Roglan <59785089+mroglan@users.noreply.github.com>
2022-10-27 11:26:15 -0400

    Testing scan code (#55)
    * adjusted red filter
    
    * fixed lidar tf
    
    * removed unused var
    
    * adjusted lower_red
    
    * wait till ready
    
    * debugging
    
    * avoiding red objects in bg
    
    * waiting for req from autonomy
    
    * not adding black twice
    
    Co-authored-by: George Burdell <gitmrobotics@gmail.com>

--- virtuoso_autonomy/virtuoso_autonomy/robotx/scan_code/main.py
+++ virtuoso_autonomy/virtuoso_autonomy/robotx/scan_code/main.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int8, Int32MultiArray
 from nav_msgs.msg import Path, Odometry
+from geometry_msgs.msg import PoseStamped
 
 class ScanCode(Node):
 
@@ -11,6 +12,8 @@ class ScanCode(Node):
         self.scan_req_pub = self.create_publisher(Int8, '/perception/get_code', 10)
         self.path_pub = self.create_publisher(Path, '/virtuoso_navigation/set_path', 10)
 
+        self.scan_ready_sub = self.create_subscription(Int8, '/perception/scan_code/ready', 
+            self.scan_ready, 10)
         self.scan_res_sub = self.create_subscription(Int32MultiArray, '/perception/code',
             self.code_callback, 10)
         self.odom_sub = self.create_subscription(Odometry, '/localization/odometry', 
@@ -18,25 +21,41 @@ class ScanCode(Node):
 
         self.req_sent = False
         self.station_keeping_enabled = False
-        self.odom = False
+        self.odom = None
+
+        self.ready = False
 
         self.create_timer(1.0, self.send_req)
+
+    def scan_ready(self, msg):
+        self.ready = True
     
     def odom_callback(self, msg):
-        if not self.odom is None:
+        # self.get_logger().info('GOT ODOM')
+        if self.odom is not None:
             return
+        self.get_logger().info('GETTING ODOM')
         self.odom = msg
-        self.enable_station_keeping()
+        self.enable_station_keeping(msg)
 
-    def enable_station_keeping(self):
+    def enable_station_keeping(self, msg):
         path = Path()
-        path.poses.append(self.odom.pose.pose)
+        pose_stamped = PoseStamped()
+        pose_stamped.pose = self.odom.pose.pose
+        path.poses.append(pose_stamped)
         self.path_pub.publish(path)
         self.station_keeping_enabled = True
         self.get_logger().info('Station Keeping Enabled')
     
     def send_req(self):
+        # self.get_logger().info('HELLO WORLD')
+        # self.get_logger().info(str(self.odom))
+        # self.get_logger().info(str(self.station_keeping_enabled))
+        # self.get_logger().info(str(self.req_sent))
         if not self.station_keeping_enabled:
+            return
+
+        if not self.ready:
             return
 
         if self.req_sent:
