e2caa881f160a0e1e27bdd90e84b9d0090463d59
Manuel Roglan <manueljoseph113@gmail.com>
2022-10-29 05:08:43 -0400

    attempted transforms

--- virtuoso_navigation/virtuoso_navigation/translate_node.py
+++ virtuoso_navigation/virtuoso_navigation/translate_node.py
@@ -2,6 +2,13 @@ import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
 from virtuoso_msgs.action import Translate
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
+from tf2_ros.buffer import Buffer
+from rclpy.time import Time, Duration
+from tf2_ros.transform_listener import TransformListener
+import time
+from virtuoso_perception.utils.geometry_msgs import do_transform_pose_stamped
 
 class TranslateActionServer(Node):
 
@@ -9,11 +16,68 @@ class TranslateActionServer(Node):
         super().__init__('translate', namespace='navigation')
 
         self._action_server = ActionServer(self, Translate, 'translate', self.execute_callback)
+
+        self.set_path_pub = self.create_publisher(Path, '/virtuoso_navigation/set_path', 10)
+
+        self.nav_success_sub = self.create_subscription(PoseStamped, '/virtuoso_navigation/success',
+            self.nav_success_callback, 10)
+
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self, spin_thread=True)
+        
+        self.goal_handle = None
+        self.nav_success = False
+        self.aborted_result = None
     
+    def nav_success_callback(self, msg):
+        self.nav_success = True
+    
+    def navigate(self):
+        future = self.tf_buffer.wait_for_transform_async('map', 'wamv/base_link', Time())
+        future.add_done_callback(self.send_path) 
+
+    def send_path(self):
+        
+        trans = None
+
+        try:
+            now = Time()
+            trans = self.tf_buffer.lookup_transform('map', 'wamv/base_link', now)
+        except Exception as e:
+            self.get_logger().info('FAILED TRANSFORM')
+            self.get_logger().info(str(e))
+            self.goal_handle.abort()
+            self.aborted_result = Translate.Result()
+            self.aborted_result.success = 0
+            return
+        
+        path = Path()
+        ps = PoseStamped()
+        ps.pose.position.x = self.goal_handle.request.x
+        ps.pose.position.y = self.goal_handle.request.y
+
+        trans_point = do_transform_pose_stamped(ps, trans)
+        path.poses.append(trans_point)
+
+        self.set_path_pub.publish(path)
+
     def execute_callback(self, goal_handle):
         self.get_logger().info('Executing action')
         self.get_logger().info(f'{goal_handle.request.x}, {goal_handle.request.y}')
 
+        self.nav_success = False
+        self.aborted_result = None
+        self.goal_handle = goal_handle
+
+        # self.send_path(goal_handle.request.x, goal_handle.request.y)
+        self.navigate()
+
+        while not self.nav_success:
+            if self.aborted_result is not None: 
+                return self.aborted_result
+            time.sleep(0.1)
+
+        self.get_logger().info('SUCCESS')
         goal_handle.succeed()
         result = Translate.Result()
         result.success = 1
