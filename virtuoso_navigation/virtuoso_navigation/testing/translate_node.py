8d38ebbcdcc070b06d9238a8ae02d9415fd89ebd
Manuel Roglan <manueljoseph113@gmail.com>
2022-10-31 19:48:34 -0400

    changed translation to topic not action

--- virtuoso_navigation/virtuoso_navigation/testing/translate_node.py
+++ virtuoso_navigation/virtuoso_navigation/testing/translate_node.py
@@ -1,23 +1,17 @@
 import rclpy
-from rclpy.action import ActionClient
 from rclpy.node import Node
-from virtuoso_msgs.action import Translate
+from geometry_msgs.msg import Point
 
 class TestTranslate(Node):
 
     def __init__(self):
         super().__init__('test_translate', namespace='navigation')
 
-        self.action_client = ActionClient(self, Translate, '/navigation/translate')
+        # self.action_client = ActionClient(self, Translate, '/navigation/translate')
+        self.pub = self.create_publisher(Point, '/navigation/translate', 10)
     
     def send_goal(self):
-        goal_msg = Translate.Goal()
-        goal_msg.x = 0.0
-        goal_msg.y = 10.0
-
-        self.action_client.wait_for_server()
-
-        return self.action_client.send_goal_async(goal_msg)
+        self.pub.publish(Point(y=2.0))
 
 
 def main(args=None):
--- virtuoso_navigation/virtuoso_navigation/translate_node.py
+++ virtuoso_navigation/virtuoso_navigation/translate_node.py
@@ -1,9 +1,8 @@
 import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
-from virtuoso_msgs.action import Translate
 from nav_msgs.msg import Path
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Point
 from tf2_ros.buffer import Buffer
 from rclpy.time import Time, Duration
 from tf2_ros.transform_listener import TransformListener
@@ -18,19 +17,26 @@ class TranslateActionServer(Node):
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
-        self._action_server = ActionServer(self, Translate, '/navigation/translate', self.execute_callback)
-
-        self.set_path_pub = self.create_publisher(Path, '/virtuoso_navigation/set_path', 10)
-
+        # self._action_server = ActionServer(self, Translate, '/navigation/translate', self.execute_callback)
+        self.translate_sub = self.create_subscription(Point, '/navigation/translate', 
+            self.execute_callback, 10)
         self.nav_success_sub = self.create_subscription(PoseStamped, '/virtuoso_navigation/success',
             self.nav_success_callback, 10)
+
+        self.set_path_pub = self.create_publisher(Path, '/virtuoso_navigation/set_path', 10)
+        self.translate_success_pub = self.create_publisher(Point, '/navigation/translate_success',
+            10)
         
-        self.goal_handle = None
-        self.nav_success = False
-        self.aborted_result = None
+        self.goal = None
+        self.trans_goal = None
     
-    def nav_success_callback(self, msg):
-        self.nav_success = True
+    def nav_success_callback(self, msg:PoseStamped):
+        if msg.pose.position.x != self.trans_goal.pose.position.x:
+            return
+        if msg.pose.position.y != self.trans_goal.pose.position.y:
+            return
+        self.get_logger().info('Completed Translation')
+        self.translate_success_pub.publish(self.goal)
     
     def send_path(self):
         
@@ -42,42 +48,23 @@ class TranslateActionServer(Node):
         except Exception as e:
             self.get_logger().info('FAILED TRANSFORM')
             self.get_logger().info(str(e))
-            self.goal_handle.abort()
-            self.aborted_result = Translate.Result()
-            self.aborted_result.success = 0
             return
         
         path = Path()
         ps = PoseStamped()
-        ps.pose.position.x = self.goal_handle.request.x
-        ps.pose.position.y = self.goal_handle.request.y
+        ps.pose.position.x = self.goal.x
+        ps.pose.position.y = self.goal.y
 
         trans_point = do_transform_pose_stamped(ps, trans)
         path.poses.append(trans_point)
 
+        self.trans_goal = trans_point
+
         self.set_path_pub.publish(path)
 
-    def execute_callback(self, goal_handle):
-        self.get_logger().info('Executing action')
-        self.get_logger().info(f'{goal_handle.request.x}, {goal_handle.request.y}')
-
-        self.nav_success = False
-        self.aborted_result = None
-        self.goal_handle = goal_handle
-
-        # self.send_path(goal_handle.request.x, goal_handle.request.y)
+    def execute_callback(self, msg:Point):
+        self.goal = msg
         self.send_path()
-
-        while not self.nav_success:
-            if self.aborted_result is not None: 
-                return self.aborted_result
-            time.sleep(0.1)
-
-        self.get_logger().info('SUCCESS')
-        goal_handle.succeed()
-        result = Translate.Result()
-        result.success = 1
-        return result
 
 
 def main(args=None):
