c82eb8e9066e9135565009352615474677345d8e
Manuel Roglan <manueljoseph113@gmail.com>
2022-11-01 16:09:19 -0400

    launching extra voxel filter from processing

--- virtuoso_perception/launch/find_docks.launch.py
+++ virtuoso_perception/launch/find_docks.launch.py
@@ -9,25 +9,11 @@ import os
 def generate_launch_description():
 
     pkg_share = get_package_share_directory('virtuoso_perception')
-
-    voxel_grid_node_param_file = os.path.join(pkg_share, 'param', 'voxel_grid_node.param.yaml')
-
-    voxel_grid_node_param = DeclareLaunchArgument(
-        'voxel_grid_node_param_file',
-        default_value=voxel_grid_node_param_file,
-        description='Path to config file for Voxel Grid Node'
-    )
+    
     return LaunchDescription([
-        voxel_grid_node_param,
         IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/euclidean_clustering.launch.py'))),
         Node(
             package='virtuoso_perception',
             executable='find_docks'
         ),
-        Node(
-            package='voxel_grid_nodes',
-            executable='voxel_grid_node_exe',
-            parameters=[LaunchConfiguration('voxel_grid_node_param_file')],
-            remappings=[('points_in', '/local_costmap/voxel_grid')]
-        )
     ])
--- virtuoso_processing/launch/lidar_processing.launch.py
+++ virtuoso_processing/launch/lidar_processing.launch.py
@@ -21,15 +21,30 @@ def generate_launch_description():
         description='Path to config file for Ray Ground Classifier'
     )
 
+    voxel_grid_node_param_file = os.path.join(pkg_share, 'param', 'voxel_grid_node.param.yaml')
+
+    voxel_grid_node_param = DeclareLaunchArgument(
+        'voxel_grid_node_param_file',
+        default_value=voxel_grid_node_param_file,
+        description='Path to config file for Voxel Grid Node'
+    )
+
     return LaunchDescription([
+        voxel_grid_node_param,
+        ray_ground_classifier_param,
 
         # running ground filter on raw LIDAR data
-        ray_ground_classifier_param,
         Node(
             package='ray_ground_classifier_nodes',
             executable='ray_ground_classifier_cloud_node_exe',
             parameters=[LaunchConfiguration('ray_ground_classifier_param_file')],
             remappings=[("points_in", "wamv/sensors/lidars/lidar_wamv/points")] # points_in comes from raw LIDAR data
+        ),
+        Node(
+            package='voxel_grid_nodes',
+            executable='voxel_grid_node_exe',
+            parameters=[LaunchConfiguration('voxel_grid_node_param_file')],
+            remappings=[('points_in', '/local_costmap/voxel_grid')]
         ),
 
         Node(
