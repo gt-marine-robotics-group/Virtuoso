b6e010a00334d964baddd9d976a57edd9806762c
Manuel Roglan <manueljoseph113@gmail.com>
2022-11-04 09:59:16 -0400

    added usv arg to find_buoys launch

--- virtuoso_perception/launch/find_buoys.launch.py
+++ virtuoso_perception/launch/find_buoys.launch.py
@@ -1,7 +1,8 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -9,9 +10,17 @@ def generate_launch_description():
 
     pkg_share = get_package_share_directory('virtuoso_perception')
 
+    usv_arg = DeclareLaunchArgument('usv')
+    usv_config = LaunchConfiguration('usv')
+
     # must have virtuoso_processing lidar_processing launched as well for euclidean clustering to do anything
     return LaunchDescription([
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/euclidean_clustering.launch.py'))),
+        usv_arg, 
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/euclidean_clustering.launch.py')),
+            launch_arguments={'usv': usv_config}.items()
+        ),
         Node(
             package='virtuoso_perception',
             executable='find_buoys'
