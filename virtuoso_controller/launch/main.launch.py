540609ef434aac8cffdda39058a736aa47cfa5ff
Manuel Roglan <manueljoseph113@gmail.com>
2022-11-05 20:48:56 -0400

    separated robotx and vrx_robotx configs

--- virtuoso_controller/launch/main.launch.py
+++ virtuoso_controller/launch/main.launch.py
@@ -4,39 +4,34 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
-import rclpy
 from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import LaunchConfiguration
 
-pkg_share = FindPackageShare(package='virtuoso_controller').find('virtuoso_controller')
-#robot_localization_file_path = os.path.join(pkg_share, 'config/ekf.yaml') 
-
-virtuoso_params_file = os.path.join(pkg_share, 'config/pidgains.yml') 
-
-#virtuoso_params_file = os.path.join(
-#  get_package_share_directory('virtuoso_controller'),
-#  'config',
-#  'pid_gains.yml'
-#)
-
-
 def generate_launch_description():
 
-    sim_time = DeclareLaunchArgument('sim_time', default_value='False')
+    pkg_share = get_package_share_directory('virtuoso_controller')
 
+    sim_time_arg = DeclareLaunchArgument('sim_time', default_value='False')
     sim_time_config = LaunchConfiguration('sim_time', default='False')
 
+    usv_arg = DeclareLaunchArgument('usv')
+    usv_config = LaunchConfiguration('usv')
+
+    pid_params_file = (pkg_share, '/config/', usv_config, '/pidgain.yaml')
+
     return LaunchDescription([
-        sim_time,
+        sim_time_arg,
+        usv_arg,
+
         Node(
             package='virtuoso_controller',
             executable='basic_pid',
-            parameters = [virtuoso_params_file]
+            parameters = [pid_params_file]
         ),
         Node(
             package='virtuoso_controller',
             executable='velocity_pid',
-            parameters = [virtuoso_params_file]
+            parameters = [pid_params_file]
         ),
         Node(
             package='virtuoso_controller',
--- virtuoso_controller/setup.py
+++ virtuoso_controller/setup.py
@@ -13,7 +13,12 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yml')))
+
+        (os.path.join('share', package_name, 'config/vrx_robotx'), 
+            glob(os.path.join('config', 'vrx_robotx', '*.yaml'))),
+
+        (os.path.join('share', package_name, 'config/robotx'),
+            glob(os.path.join('config', 'robotx', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
