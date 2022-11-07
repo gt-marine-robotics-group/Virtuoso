63876e731bc116d812ca88bdd043794ea3ba71f0
Manuel Roglan <59785089+mroglan@users.noreply.github.com>
2022-11-07 15:36:23 -0500

    Physical transforms (#70)
    * added lidar tf
    
    * added gps and imu transforms
    
    * added tf to camera
    
    * added usv config to sensor main

--- virtuoso_sensors/launch/main.launch.py
+++ virtuoso_sensors/launch/main.launch.py
@@ -5,15 +5,34 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
 import os
 
 def generate_launch_description():
 
     pkg_share = get_package_share_directory('virtuoso_sensors')
 
+    usv_arg = DeclareLaunchArgument('usv')
+    usv_config = LaunchConfiguration('usv')
+
     return LaunchDescription([
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/lidar.launch.py'))),
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/gps.launch.py'))),
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/imu.launch.py'))),
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/camera.launch.py')))
+        usv_arg,
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/lidar.launch.py')),
+            launch_arguments={'usv': usv_config}.items()
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/gps.launch.py')),
+            launch_arguments={'usv': usv_config}.items()
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/imu.launch.py')),
+            launch_arguments={'usv': usv_config}.items()
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/camera.launch.py')),
+            launch_arguments={'usv': usv_config}.items()
+        )
     ])
