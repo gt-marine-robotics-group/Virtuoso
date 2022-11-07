d663c25e28faeafc0f9d406a9bfedd390d3ce74d
Manuel Roglan <manueljoseph113@gmail.com>
2022-11-07 14:55:39 -0500

    added tf to camera

--- virtuoso_sensors/launch/camera.launch.py
+++ virtuoso_sensors/launch/camera.launch.py
@@ -1,10 +1,18 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.conditions import IfCondition
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    
+    usv_arg = DeclareLaunchArgument('usv')
+    usv_config = LaunchConfiguration('usv')
 
     return LaunchDescription([
+        usv_arg,
+
         Node(
             package='usb_cam',
             executable='usb_cam_node_exe'
@@ -12,5 +20,12 @@ def generate_launch_description():
         Node(
             package='virtuoso_sensors',
             executable='camera_republish'
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='leftcam_to_baselink',
+            arguments=['0', '0', '1.245', '0', '0', '0', 'wamv/base_link', 'wamv/front_left_camera_link_optical'],
+            condition=IfCondition(PythonExpression(["'", usv_config, "' == 'robotx'"]))
         )
     ])
