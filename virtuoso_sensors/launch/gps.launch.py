ecef1defb592ff2941c0b1e80295133277556813
Manuel Roglan <manueljoseph113@gmail.com>
2022-11-07 14:47:42 -0500

    added gps and imu transforms

--- virtuoso_sensors/launch/gps.launch.py
+++ virtuoso_sensors/launch/gps.launch.py
@@ -3,14 +3,20 @@ from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.conditions import IfCondition
 import os
 
 def generate_launch_description():
 
     sensors = get_package_share_directory('virtuoso_sensors')
 
+    usv_arg = DeclareLaunchArgument('usv')
+    usv_config = LaunchConfiguration('usv')
+
     return LaunchDescription([
-        #IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(processing, 'launch', 'main.launch.py'))),
+        usv_arg,
         Node(
             package='virtuoso_sensors',
             executable='f9p_gps_republish'
@@ -19,13 +25,15 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             name='imu_to_baselink',
-            arguments=['0', '0', '0', '0', '0', '0', 'wamv/base_link', 'imu_frame']
+            arguments=['0', '0', '1.245', '0', '0', '0', 'wamv/base_link', 'imu_frame'],
+            condition=IfCondition(PythonExpression(["'", usv_config, "' == 'robotx'"]))
         ),
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='gps_to_baselink',
-            arguments=['0', '0', '0', '0', '0', '0', 'wamv/base_link', 'ubx']
+            arguments=['0', '0', '1.245', '0', '0', '0', 'wamv/base_link', 'ubx'],
+            condition=IfCondition(PythonExpression(["'", usv_config, "' == 'robotx'"]))
         ),
         IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(sensors, 'launch', 'f9p.launch.py'))),
     ])
