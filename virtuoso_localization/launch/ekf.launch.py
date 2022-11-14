83884e130e5eb188dca0b57f71f78f20e41eed35
George Burdell <gitmrobotics@gmail.com>
2022-11-14 01:36:44 -0500

    fixed orientation

--- virtuoso_localization/launch/ekf.launch.py
+++ virtuoso_localization/launch/ekf.launch.py
@@ -56,12 +56,12 @@ def generate_launch_description():
                 {"zero_altitude": False},
                 {"yaw_offset": 0.0},
                 {"use_odometry_yaw": True},
-                #{"magnetic_declination_radians": 0.33929201},
-                #{"magnetic_declination_radians": 0.0},
+                # {"magnetic_declination_radians": 0.33929201},
+                #{"magnetic_declination_radians": 0.222},
                 {"delay": 0.0},
                 {"frequency": 30.0},
                 {"broadcast_utm_transform": True},
-                robot_localization_file_path
+                # robot_localization_file_path
             ]
         )
     ])
