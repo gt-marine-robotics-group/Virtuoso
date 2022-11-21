a3850dfdb1a20305c073b4ac68ba2f00bbb39bde
Manuel Roglan <manueljoseph113@gmail.com>
2022-11-20 22:49:07 -0500

    removed unused vars

--- virtuoso_autonomy/launch/robotx/robotx_dock_setup.launch.py
+++ virtuoso_autonomy/launch/robotx/robotx_dock_setup.launch.py
@@ -1,15 +1,11 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch.conditions import UnlessCondition
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-
-    pkg_share = get_package_share_directory('virtuoso_autonomy')
 
     sim_time = DeclareLaunchArgument('sim_time', default_value='False')
     usv = DeclareLaunchArgument('usv')
@@ -22,18 +18,10 @@ def generate_launch_description():
     localization = get_package_share_directory('virtuoso_localization')
     perception = get_package_share_directory('virtuoso_perception')
     controller = get_package_share_directory('virtuoso_controller')
-    sensors = get_package_share_directory('virtuoso_sensors')
-
-    docking_config_file = (pkg_share, '/config/', usv_config, '/docking.yaml')
 
     return LaunchDescription([
         sim_time,
         usv,
-
-        # IncludeLaunchDescription(
-        #     PythonLaunchDescriptionSource(os.path.join(sensors, 'launch', 'main.launch.py')),
-        #     condition=UnlessCondition(sim_time_config)
-        # ),
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(os.path.join(processing, 'launch', 'main.launch.py')),
@@ -55,10 +43,4 @@ def generate_launch_description():
             PythonLaunchDescriptionSource(os.path.join(controller, 'launch', 'main.launch.py')),
             launch_arguments={'sim_time': sim_time_config, 'usv': usv_config}.items()
         ),
-
-        # Node(
-        #     package='virtuoso_autonomy',
-        #     executable='robotX_docking',
-        #     parameters=[docking_config_file]
-        # )
     ])
