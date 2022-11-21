c789b7541d0eab3264ae18a3a6f5c7ceed398b4b
Manuel Roglan <manueljoseph113@gmail.com>
2022-11-21 00:42:05 -0500

    removed unused imports and comments

--- virtuoso_perception/launch/scan_code.launch.py
+++ virtuoso_perception/launch/scan_code.launch.py
@@ -1,8 +1,7 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
