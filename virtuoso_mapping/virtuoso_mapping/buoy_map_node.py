b3f740df1c3b44c1eedfe56f27ce1b233b587546
Manuel Roglan <manueljoseph113@gmail.com>
2022-12-14 22:29:40 -0500

    getting buoy angle to camera

--- virtuoso_mapping/virtuoso_mapping/buoy_map_node.py
+++ virtuoso_mapping/virtuoso_mapping/buoy_map_node.py
@@ -1,15 +1,19 @@
 import rclpy
 from rclpy.node import Node
-from virtuoso_msgs.msg import BuoyArray
-from geometry_msgs.msg import PointStamped
+from virtuoso_msgs.msg import BuoyArray, Buoy
+from geometry_msgs.msg import PointStamped, Pose, Point, Quaternion, Vector3
 from sensor_msgs.msg import PointCloud2
-from virtuoso_perception.utils.geometry_msgs import do_transform_point
+from virtuoso_perception.utils.geometry_msgs import do_transform_point, do_transform_pose
 from virtuoso_processing.utils.pointcloud import create_cloud_xyz32
 from collections import deque
 from tf2_ros import TransformStamped
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 from rclpy.time import Time
+from .mapped_buoy import MappedBuoy
+from typing import List
+import math
+import tf_transformations
 
 class BuoyMapNode(Node):
 
@@ -26,32 +30,33 @@ class BuoyMapNode(Node):
             '/perception/stereo/buoys', self.buoys_callback, 10)
         
         self.debug_pubs = {
-            '/mapping/debug/raw_buoy_points': self.create_publisher(PointCloud2, 
-                '/mapping/debug/raw_buoy_points', 10)
+            '/mapping/debug/filtered_buoy_points': self.create_publisher(PointCloud2, 
+                '/mapping/debug/filtered_buoy_points', 10)
         }
 
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
-        
-        self.cam_transform = None
+
+        self.left_cam_to_map_transform:TransformStamped = None
+        self.left_camera_pose:Pose = None
+        self.right_camera_pose:Pose = None
         
         self.curr_buoys:BuoyArray = None
 
-        self.prev_buoys = deque(maxlen=10)
+        self.mapped_buoys:List[MappedBuoy] = list()
 
-    def find_cam_transform(self):
-        trans:TransformStamped = None
-        try:
-            trans = self.tf_buffer.lookup_transform(
-                self.frames[0],
-                self.frames[1],
-                Time()
-            )
-        except:
-            self.get_logger().info('Transform failed')
-            return
+    def find_camera_poses(self):
+        right_to_map_trans:TransformStamped = self.tf_buffer.lookup_transform(
+            'map',
+            BuoyMapNode.frames[1],
+            Time()
+        )
+
+        start = Pose(position=Point(x=0.0,y=0.0,z=0.0), 
+            orientation=Quaternion(x=0.0,y=0.0,z=0.0,w=1.0))
         
-        self.cam_transform = trans
+        self.left_camera_pose = do_transform_pose(start, self.left_cam_to_map_transform)
+        self.right_camera_pose = do_transform_pose(start, right_to_map_trans)
 
     def buoys_callback(self, msg:BuoyArray):
         self.curr_buoys = msg
@@ -64,6 +69,8 @@ class BuoyMapNode(Node):
                 BuoyMapNode.frames[0],
                 Time()
         )
+
+        self.left_cam_to_map_transform = trans
 
         for buoy in self.curr_buoys.buoys:
             ps = PointStamped(point=buoy.location) 
@@ -79,16 +86,79 @@ class BuoyMapNode(Node):
         
         pcd = create_cloud_xyz32(pcd.header, pcd_points)
 
-        self.debug_pubs['/mapping/debug/raw_buoy_points'].publish(pcd)
+        self.debug_pubs['/mapping/debug/filtered_buoy_points'].publish(pcd)
     
+    def buoy_distance(buoy:Buoy):
+        return math.sqrt(buoy.location.x**2 + buoy.location.y**2)
+    
+    def find_euler(quat:Quaternion):
+        q = [quat.x, quat.y, quat.z, quat.w]
+
+        euler = tf_transformations.euler_from_quaternion(q)
+
+        return Vector3(x=euler[0], y=euler[1], z=euler[2])
+    
+    def filter_curr_buoys(self):
+        index = 0
+        while index < len(self.curr_buoys.buoys):
+            if (BuoyMapNode.buoy_distance(self.curr_buoys.buoys[index]) > 15):
+                self.curr_buoys.buoys.pop(index)
+            else:
+                index += 1
+    
+    def is_buoy_in_view(self, buoy:MappedBuoy, cam_location:Point, 
+        cam_orientation:Vector3):
+
+        cam_2d_vector = Vector3(y=math.sin(cam_orientation.z), 
+            x=math.cos(cam_orientation.z))
+        
+        buoy_2d_vector = Vector3(x=buoy.location.x - cam_location.x,
+            y=buoy.location.y - cam_location.y)
+        
+        c = cam_2d_vector
+        b = buoy_2d_vector 
+        angle = math.acos(((c.x * b.x) + (c.y * b.y)) / 
+            (math.sqrt(c.x**2 + c.y**2) * math.sqrt(b.x**2 + b.y**2)))
+        
+        self.get_logger().info(f'angle: {angle * 180 / math.pi}') 
+
+        return abs(angle) <= BuoyMapNode.cam_fov
+
+    def mapped_buoys_in_view(self):
+        left_cam_orientation = BuoyMapNode.find_euler(self.left_camera_pose.orientation)
+        right_cam_orientation = BuoyMapNode.find_euler(self.right_camera_pose.orientation)
+
+        for buoy in self.curr_buoys.buoys:
+            self.is_buoy_in_view(buoy, self.left_camera_pose.position, 
+                left_cam_orientation)
+    
+    def update_mapped_buoys(self):
+        try:
+            self.find_camera_poses()
+        except Exception as e:
+            self.get_logger().info('Camera pose transform failed')
+            self.get_logger().info(str(e))
+            return
+        
+        unmapped_indeces = set(i for i in range(len(self.curr_buoys.buoys)))
+
+        self.mapped_buoys_in_view()
+
     def execute(self):
+
+        self.filter_curr_buoys()
+
+        camera_buoy_locs = self.curr_buoys.buoys.copy()
 
         try:
             self.transform_curr_buoys_to_map_frame()
         except Exception:
+            self.get_logger().info('Camera to map transform failed')
             return
 
         self.debug_pcd(self.curr_buoys)
+
+        self.update_mapped_buoys()
 
 
 def main(args=None):
---
+++ virtuoso_mapping/virtuoso_mapping/mapped_buoy.py
@@ -0,0 +1,29 @@
+from geometry_msgs.msg import Point
+from virtuoso_msgs.msg import Buoy
+
+class MappedBuoy:
+    
+    def __init__(self, buoy:Buoy):
+        self.location = buoy.location
+        self.color = buoy.color
+
+        self.count = 1
+    
+    def compute_new_avg(current:float, new:float, weight:float):
+        return (current * (1 - weight)) + (new * weight)
+    
+    def add_detected_buoy(self, buoy:Buoy, distance_from_usv:float):
+        if self.color != buoy.color:
+            raise Exception('Buoy color cannot change')
+        
+        self.count += 1
+        
+        weight = 1 / distance_from_usv
+
+        if weight > 1: weight = 1
+
+        self.location.x = MappedBuoy.compute_new_avg(self.location.x, 
+            buoy.location.x, weight)
+        self.location.y = MappedBuoy.compute_new_avg(self.location.y, 
+            buoy.location.y, weight)
+        
