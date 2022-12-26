6e04fa48f70326792b5211e13154955ad06d774a
Manuel Roglan <manueljoseph113@gmail.com>
2022-12-26 14:12:27 -0500

    removed mapping from safety check launch

--- virtuoso_autonomy/launch/robotx/robotx_safety_check_setup.launch.py
+++ virtuoso_autonomy/launch/robotx/robotx_safety_check_setup.launch.py
@@ -19,7 +19,7 @@ def generate_launch_description():
     localization = get_package_share_directory('virtuoso_localization')
     perception = get_package_share_directory('virtuoso_perception')
     controller = get_package_share_directory('virtuoso_controller')
-    mapping = get_package_share_directory('virtuoso_mapping')
+    # mapping = get_package_share_directory('virtuoso_mapping')
 
     return LaunchDescription([
         sim_time_arg,
@@ -37,9 +37,9 @@ def generate_launch_description():
             PythonLaunchDescriptionSource(os.path.join(navigation, 'launch', 'main.launch.py')),
             launch_arguments={'sim_time': sim_time_config, 'usv': usv_config}.items()
         ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(mapping, 'launch', 'main.launch.py')),
-        ),
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource(os.path.join(mapping, 'launch', 'main.launch.py')),
+        # ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(os.path.join(perception, 'launch', 'find_buoys.launch.py')),
             launch_arguments={'usv': usv_config}.items()
