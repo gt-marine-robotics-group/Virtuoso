fe6a80488b46cce8a16bfc42fa6d7b9550592f41
Manuel Roglan <manueljoseph113@gmail.com>
2023-01-15 09:42:51 -0500

    added activate to buoy_lidar

--- virtuoso_perception/virtuoso_perception/buoys/buoy_lidar_node.py
+++ virtuoso_perception/virtuoso_perception/buoys/buoy_lidar_node.py
@@ -13,8 +13,8 @@ class BuoyLidarNode(Node):
             self.lidar_bounding_boxes_callback, 10)
         self.boxes_pub = self.create_publisher(BoundingBoxArray, '/buoys/bounding_boxes', 10)
 
-        # self.activate_sub = self.create_subscription(Bool, 
-        #     '/perception/buoys/')
+        self.activate_sub = self.create_subscription(Bool, 
+            '/perception/buoys/buoy_lidar/activate', self.activate_callback, 10)
 
         self.declare_parameters(namespace='', parameters=[
             ('buoy_max_side_length', 0.0),
@@ -22,20 +22,29 @@ class BuoyLidarNode(Node):
             ('buoy_loc_noise', 0.0)
         ])
 
-        self.find_buoys = FindBuoys(
-            buoy_max_side_length=self.get_parameter('buoy_max_side_length').value,
-            tall_buoy_min_z=self.get_parameter('tall_buoy_min_z').value,
-            buoy_loc_noise=self.get_parameter('buoy_loc_noise').value
-        )
+        self.find_buoys:FindBuoys = None
 
         self.create_timer(0.1, self.execute)
     
+    def activate_callback(self, msg:Bool):
+        if msg.data:
+            self.find_buoys = FindBuoys(
+                buoy_max_side_length=self.get_parameter('buoy_max_side_length').value,
+                tall_buoy_min_z=self.get_parameter('tall_buoy_min_z').value,
+                buoy_loc_noise=self.get_parameter('buoy_loc_noise').value
+            )
+        else:
+            self.find_buoys = None
+
     def execute(self):
+        if self.find_buoys is None:
+            return
         buoys = self.find_buoys.find_buoys()
         self.boxes_pub.publish(buoys)
 
     def lidar_bounding_boxes_callback(self, msg:BoundingBoxArray):
-        self.find_buoys.lidar_bounding_boxes = msg
+        if self.find_buoys is not None:
+            self.find_buoys.lidar_bounding_boxes = msg
     
 
 def main(args=None):
