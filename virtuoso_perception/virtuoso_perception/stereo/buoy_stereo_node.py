5b9dabb5f9ceacd79c7460cb19bf973e1d758fb9
Manuel Roglan <manueljoseph113@gmail.com>
2023-01-30 14:22:47 -0500

    started buoy stereo refactoring

--- virtuoso_perception/virtuoso_perception/stereo/buoy_stereo_node.py
+++ virtuoso_perception/virtuoso_perception/stereo/buoy_stereo_node.py
@@ -24,16 +24,6 @@ class BuoyStereoNode(Node):
         self.frames = self.get_parameter('frames').value
         base_topics = self.get_parameter('base_topics').value
 
-        self.left_buoy_contours_sub = self.create_subscription(Contours,
-            f'{base_topics[0]}/buoy_filter', self.left_buoy_contours_callback, 10)
-        self.left_cam_info_sub = self.create_subscription(CameraInfo, 
-            f'{base_topics[0]}/resized/camera_info', self.left_cam_info_callback, 10)
-        
-        self.right_buoy_contours_sub = self.create_subscription(Contours,
-            f'{base_topics[1]}/buoy_filter', self.right_buoy_contours_callback, 10)
-        self.right_cam_info_sub = self.create_subscription(CameraInfo,
-            f'{base_topics[1]}/resized/camera_info', self.right_cam_info_callback, 10)
-        
         self.debug_pubs = {
             '/perception/stereo/debug/left_cam/contoured_buoy': [
                 self.create_publisher(Image, '/perception/stereo/debug/left_cam/contoured_buoy1', 10)
@@ -54,12 +44,8 @@ class BuoyStereoNode(Node):
                 '/perception/stereo/debug/points', 10)
         }
 
-        self.buoys_pub = self.create_publisher(BuoyArray, '/perception/stereo/buoys', 10)
-
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
-
-        self.prev_timestamp = None
 
         if self.get_parameter('debug').value:
             node = self
@@ -67,8 +53,6 @@ class BuoyStereoNode(Node):
             node = None
         self.buoy_stereo = BuoyStereo(node=node,
             multiprocessing=self.get_parameter('multiprocessing').value)
-
-        self.create_timer(1.0, self.execute_buoy_stereo)
     
     def update_debug_pub_sizes(self, size:int):
         for base_name, pubs in self.debug_pubs.items():
@@ -86,18 +70,6 @@ class BuoyStereoNode(Node):
     def debug_pub(self, name:str, msg):
         self.single_debug_pubs[name].publish(msg)
     
-    def left_buoy_contours_callback(self, msg:Contours):
-        self.buoy_stereo.left_img_contours = msg
-    
-    def right_buoy_contours_callback(self, msg:Contours):
-        self.buoy_stereo.right_img_contours = msg
-
-    def left_cam_info_callback(self, msg:CameraInfo):
-        self.buoy_stereo.left_cam_info = msg
-    
-    def right_cam_info_callback(self, msg:CameraInfo):
-         self.buoy_stereo.right_cam_info = msg
-        
     def find_cam_transform(self):
         trans:TransformStamped = None
         try:
@@ -115,26 +87,20 @@ class BuoyStereoNode(Node):
         self.buoy_stereo.cam_transform = cv2_trans
         
     def execute_buoy_stereo(self):
-        if (self.buoy_stereo.left_img_contours is None or
-            self.prev_timestamp == self.buoy_stereo.left_img_contours.header.stamp):
-            self.get_logger().info('old data')
-            return
-
-        if self.buoy_stereo.left_img_contours:
-            self.prev_timestamp = self.buoy_stereo.left_img_contours.header.stamp
 
         if self.buoy_stereo.cam_transform is None:
             self.find_cam_transform()
-            return
+            return BuoyArray()
         
         self.buoy_stereo.run()
 
         if self.buoy_stereo.buoys is None:
-            return
+            return BuoyArray()
         
         self.buoy_stereo.buoys.header.stamp = self.get_clock().now().to_msg()
         self.buoy_stereo.buoys.header.frame_id = self.frames[0]
-        self.buoys_pub.publish(self.buoy_stereo.buoys)
+
+        return self.buoy_stereo.buoys
 
 
 def main(args=None):
