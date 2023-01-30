1907c44873f9c1ba537f20fadc179990869ab12e
Manuel Roglan <manueljoseph113@gmail.com>
2023-01-30 12:35:11 -0500

    updated buoy_filter_node

--- virtuoso_perception/virtuoso_perception/buoys/buoy_filter_node.py
+++ virtuoso_perception/virtuoso_perception/buoys/buoy_filter_node.py
@@ -1,25 +1,27 @@
 import rclpy
+from rclpy.client import Client
 from rclpy.node import Node
-from virtuoso_perception.utils.ColorFilter import ColorFilter
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-import cv2
 from ..utils.code_identification import find_contours
 from ..utils.color_range import ColorRange
-import numpy as np
-from scipy import stats
-import random
-from virtuoso_msgs.msg import Contours
+from ..utils.image_srv_chain import ImageSrvChain
 from .buoy_filter import BuoyFilter
+from virtuoso_msgs.srv import ImageNoiseFilter, ImageResize, ImageBuoyFilter
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
+import time
 
 class BuoyColorFilterNode(Node):
 
     def __init__(self):
         super().__init__('perception_buoy_filter')
 
+        self.cb_group_1 = MutuallyExclusiveCallbackGroup()
+        self.cb_group_2 = MutuallyExclusiveCallbackGroup()
+
         self.declare_parameters(namespace='', parameters=[
             ('debug', False),
-            ('base_topic', ''), # comes from camera_config.yaml
 
             ('filter_bounds.red.lower1', [0,0,0]),
             ('filter_bounds.red.upper1', [0,0,0]),
@@ -47,6 +49,16 @@ class BuoyColorFilterNode(Node):
             ('buoy_px_color_sample_size', 0)
         ])
 
+        self.srv = self.create_service(ImageBuoyFilter, 'perception/image_buoy_filter',
+            self.srv_callback, callback_group=self.cb_group_1)
+
+        self.image_srv_chain = ImageSrvChain([
+            self.create_client(ImageNoiseFilter, 'perception/image_noise_filter', 
+                callback_group=self.cb_group_2),
+            self.create_client(ImageResize, 'perception/image_resize', 
+                callback_group=self.cb_group_2)
+        ])
+
         self.buoy_filter = BuoyFilter(color_filter_bounds=ColorRange(self, 
             ['red', 'green', 'black', 'yellow'], prefix='filter_bounds.'), 
             color_label_bounds=ColorRange(self, 
@@ -57,21 +69,13 @@ class BuoyColorFilterNode(Node):
         if self.get_parameter('debug').value:
             self.buoy_filter.node = self
 
-        base_topic = self.get_parameter('base_topic').value
-
-        self.image_sub = self.create_subscription(Image, 
-            f'{base_topic}/resized', self.image_callback, 10)
-        
-        self.filter_pub = self.create_publisher(Contours,
-            f'{base_topic}/buoy_filter', 10)
-        
         self.debug_pubs = {
             'black_white': self.create_publisher(Image,
-            f'{base_topic}/buoy_filter/debug/black_white', 10),
+            f'camera/buoy_filter/debug/black_white', 10),
             'full_contours': self.create_publisher(Image,
-            f'{base_topic}/buoy_filter/debug/full_contours', 10),
+            f'camera/buoy_filter/debug/full_contours', 10),
             'filtered_contours': self.create_publisher(Image,
-            f'{base_topic}/buoy_filter/debug/filtered_contours', 10)
+            f'camera/buoy_filter/debug/filtered_contours', 10)
         }
 
     def apply_filter(self, img:Image):
@@ -84,19 +88,38 @@ class BuoyColorFilterNode(Node):
 
         return self.buoy_filter.contours
     
-    def image_callback(self, msg:Image):
-        self.filter_pub.publish(self.apply_filter(msg))
+    def srv_callback(self, req:ImageBuoyFilter.Request, res:ImageBuoyFilter.Response):
+
+        if req.image is None or req.camera_info is None:
+            return res
+        
+        self.image_srv_chain.image = req.image
+        self.image_srv_chain.camera_info = req.camera_info
+        self.image_srv_chain.run()
+
+        while self.image_srv_chain.running:
+            self.get_logger().info('waiting for image_srv_chain to finish')
+            time.sleep(0.5) 
+        
+        res.contours = self.apply_filter(self.image_srv_chain.image)
+        res.camera_info = self.image_srv_chain.camera_info
+
+        return res
 
 
 def main(args=None):
     
     rclpy.init(args=args)
 
-    sub = BuoyColorFilterNode()
+    node = BuoyColorFilterNode()
 
-    rclpy.spin(sub)
+    executor = MultiThreadedExecutor(num_threads=2)
+    executor.add_node(node)
+    executor.spin()
 
-    sub.destroy_node()
+    # rclpy.spin(sub)
+
+    # sub.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
