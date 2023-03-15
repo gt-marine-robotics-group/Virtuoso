258ba96c8f14016828c00df0424465c22a55e592
Manuel Roglan <manueljoseph113@gmail.com>
2023-03-15 12:56:38 -0400

    attempting to center

--- virtuoso_autonomy/virtuoso_autonomy/roboboat/docking/docking_node.py
+++ virtuoso_autonomy/virtuoso_autonomy/roboboat/docking/docking_node.py
@@ -5,6 +5,9 @@ from nav_msgs.msg import Path, Odometry
 from std_msgs.msg import Empty
 from geometry_msgs.msg import PoseStamped, Point, Pose
 from virtuoso_msgs.srv import DockCodesCameraPos
+import time
+
+TARGET_COLOR = 'red'
 
 class DockingNode(Node):
 
@@ -32,11 +35,12 @@ class DockingNode(Node):
     def odom_callback(self, msg:Odometry):
         self.robot_pose = msg.pose.pose
     
-    def nav_success_callback(self):
+    def nav_success_callback(self, msg:PoseStamped):
         if self.state == State.APPROACHING_DOCK: self.state = State.ORIENTING
         elif self.state == State.SEARCH_TRANSLATING: self.state = State.SEARCHING_FOR_DOCK_CODE
         elif self.state == State.CENTER_TRANSLATING: self.state = State.CENTERING
         elif self.state == State.DOCKING: self.state = State.COUNTING_CODE
+        time.sleep(1.0)
     
     def execute(self):
         self.get_logger().info(str(self.state))
@@ -53,6 +57,13 @@ class DockingNode(Node):
             return
         if self.state == State.SEARCHING_FOR_DOCK_CODE:
             self.search_for_dock_code()
+            return
+        if self.state == State.SEARCH_TRANSLATING:
+            return
+        if self.state == State.CENTERING:
+            self.search_for_dock_code()
+            return
+        if self.state == State.CENTER_TRANSLATING:
             return
     
     def enable_station_keeping(self):
@@ -74,6 +85,92 @@ class DockingNode(Node):
         self.code_pos_req = None
 
         self.get_logger().info(f'result: {result}')
+
+        if len(result.red) == 0:
+            self.get_logger().info(f'No dock code positions sent')
+            return
+        
+        targetX = -1
+        othersX = 0
+        othersCount = 0
+
+        if result.red[0] != -1 and result.red[1] != -1:
+            mid = (result.red[0] + result.red[1]) // 2
+            if TARGET_COLOR == 'red':
+                targetX = mid
+            else:
+                othersX += mid
+                othersCount += 1
+        
+        if result.blue[0] != -1 and result.blue[1] != -1:
+            mid = (result.blue[0] + result.blue[1]) // 2
+            if TARGET_COLOR == 'blue':
+                targetX = mid
+            else:
+                othersX += mid
+                othersCount += 1
+        
+        if result.green[0] != -1 and result.green[1] != -1:
+            mid = (result.green[0] + result.green[1]) // 2
+            if TARGET_COLOR == 'green':
+                targetX = mid
+            else:
+                othersX += mid
+                othersCount += 1
+        
+        if othersCount > 0:
+            othersX //= othersCount
+        
+        if self.state == State.CENTERING and targetX == -1:
+            self.get_logger().info('Centering but code not found.')
+            return
+        
+        if targetX > -1:
+            self.center_translate(targetX, result.image_width)
+            return
+        
+        if othersCount == 0:
+            self.get_logger().info('No code found in search.')
+            return
+        
+        self.search_translate(othersX, result.image_width)
+    
+    def center_translate(self, targetX, image_width):
+        msg = Point()
+
+        self.state = State.CENTER_TRANSLATING
+
+        if targetX >= image_width * .45 and targetX <= image_width * .55:
+            self.get_logger().info('Within bounds, good to dock')
+            self.state = State.DOCKING
+            return
+        
+        if targetX >= image_width * .2 and targetX < image_width * .45:
+            self.get_logger().info('going left')
+            msg.y = 1.0
+        elif targetX < image_width * .2:
+            self.get_logger().info('going left')
+            msg.y = 2.0
+        elif targetX > image_width * .55 and targetX <= image_width * .8:
+            self.get_logger().info('going right')
+            msg.y = -1.0
+        else:
+            self.get_logger().info('going right')
+            msg.y = -2.0
+
+        self.trans_pub.publish(msg)
+    
+    def search_translate(self, othersX, image_width):
+        msg = Point()
+
+        self.state = State.SEARCH_TRANSLATING
+
+        if othersX < image_width * .5:
+            msg.y = 1.0
+        else:
+            msg.y = -1.0
+
+        self.trans_pub.publish(msg)
 
 
 def main(args=None):
