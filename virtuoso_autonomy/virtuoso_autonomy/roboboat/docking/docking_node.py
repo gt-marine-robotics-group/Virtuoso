55ba3174ed966c05e7cec595c113cf3881f89c99
Manuel Roglan <manueljoseph113@gmail.com>
2023-03-16 19:25:31 -0400

    some progress on orientation

--- virtuoso_autonomy/virtuoso_autonomy/roboboat/docking/docking_node.py
+++ virtuoso_autonomy/virtuoso_autonomy/roboboat/docking/docking_node.py
@@ -4,10 +4,10 @@ from .docking_states import State
 from nav_msgs.msg import Path, Odometry
 from std_msgs.msg import Empty
 from geometry_msgs.msg import PoseStamped, Point, Pose
-from virtuoso_msgs.srv import DockCodesCameraPos, CountDockCodes
+from virtuoso_msgs.srv import DockCodesCameraPos, CountDockCodes, Rotate
 import time
 
-TARGET_COLOR = 'red'
+TARGET_COLOR = 'red' # PARAM
 
 class DockingNode(Node):
 
@@ -28,6 +28,9 @@ class DockingNode(Node):
 
         self.count_code_client = self.create_client(CountDockCodes, 'count_dock_codes')
         self.count_code_req = None
+
+        self.rotate_client = self.create_client(Rotate, 'rotate')
+        self.rotate_req = None
         
         self.state = State.START
 
@@ -56,6 +59,9 @@ class DockingNode(Node):
             return
         if self.state == State.APPROACHING_DOCK:
             return
+        if self.state == State.FINDING_ORIENTATION:
+            self.find_orientation()
+            return
         if self.state == State.ORIENTING:
             return
         if self.state == State.SEARCHING_FOR_DOCK_CODE:
@@ -73,7 +79,36 @@ class DockingNode(Node):
         if self.state == State.COUNTING_CODE:
             self.count_code()
             return
+        
+    def find_orientation(self):
+        if self.code_pos_req is not None or self.rotate_req is not None:
+            return
+        
+        # use different request which gets the position of the codes using 
+        # stereo instead of their relative placements
+        # also make sure the stereo is using bounds that don't border the camera
+        msg = DockCodesCameraPos.Request()
+
+        self.code_pos_req = self.code_pos_client.call_async(msg)
+        self.code_pos_req.add_done_callback(self.orientation_code_pos_callback)
     
+    def orientation_code_pos_callback(self, future):
+        result = future.result()
+
+        self.code_pos_req = None
+
+        self.get_logger().info(f'result: {result}')
+
+        msg = Rotate.Request()
+
+        if len(result.red) == 0:
+            self.get_logger().info(f'No dock code positions sent')
+            msg.goal.z = 0.523599 # PARAM
+            return
+        
+        leftX = None
+        rightX = None
+        
     def enable_station_keeping(self):
         self.state = State.STATION_KEEPING_ENABLED
         self.station_keeping_pub.publish(Empty())
@@ -148,22 +183,22 @@ class DockingNode(Node):
 
         self.state = State.CENTER_TRANSLATING
 
-        if targetX >= image_width * .48 and targetX <= image_width * .52:
+        if targetX >= image_width * .48 and targetX <= image_width * .52: # PARAM
             self.dock()
             return
         
-        if targetX >= image_width * .2 and targetX < image_width * .48:
+        if targetX >= image_width * .2 and targetX < image_width * .48: # PARAM
             self.get_logger().info('going left')
-            msg.y = 1.0
-        elif targetX < image_width * .2:
+            msg.y = 1.0 # PARAM
+        elif targetX < image_width * .2: # PARAM
             self.get_logger().info('going left')
-            msg.y = 4.0
-        elif targetX > image_width * .52 and targetX <= image_width * .8:
+            msg.y = 4.0 # PARAM
+        elif targetX > image_width * .52 and targetX <= image_width * .8: # PARAM
             self.get_logger().info('going right')
-            msg.y = -1.0
+            msg.y = -1.0 # PARAM
         else:
             self.get_logger().info('going right')
-            msg.y = -4.0
+            msg.y = -4.0 # PARAM
 
         self.trans_pub.publish(msg)
     
@@ -172,10 +207,10 @@ class DockingNode(Node):
 
         self.state = State.SEARCH_TRANSLATING
 
-        if othersX < image_width * .5:
-            msg.y = 5.0
+        if othersX < image_width * .5: # PARAM
+            msg.y = 5.0 # PARAM
         else:
-            msg.y = -5.0
+            msg.y = -5.0 # PARAM
 
         self.trans_pub.publish(msg)
     
@@ -183,7 +218,7 @@ class DockingNode(Node):
         self.get_logger().info('Within bounds, good to dock')
         self.state = State.DOCKING
         msg = Point()
-        msg.x = 8.0
+        msg.x = 8.0 # PARAM
 
         self.trans_pub.publish(msg)
     
