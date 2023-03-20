b8ba3b603e59d3fd4bab6be222b4dbf28f88ab6b
Manuel Roglan <manueljoseph113@gmail.com>
2023-03-20 10:05:50 -0400

    navigating then shooting

--- virtuoso_autonomy/virtuoso_autonomy/roboboat/ball_shooter/ball_shooter_node.py
+++ virtuoso_autonomy/virtuoso_autonomy/roboboat/ball_shooter/ball_shooter_node.py
@@ -1,10 +1,36 @@
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import PoseStamped
+from .ball_shooter_states import State
 
 class BallShooterNode(Node):
 
     def __init__(self):
         super().__init__('autonomy_ball_shooter')
+
+        self.waypoint_player_success_sub = self.create_subscription(PoseStamped,
+            '/navigation/waypoint_player/success', self.waypoint_player_success_callback, 10)
+
+        self.state = State.NAVIGATING
+
+        self.create_timer(1.0, self.execute)
+    
+    def waypoint_player_success_callback(self, msg:PoseStamped):
+        self.get_logger().info('Waypoint player finished')
+
+        self.state = State.SHOOTING
+    
+    def execute(self):
+        self.get_logger().info(str(self.state))
+
+        if self.state == State.NAVIGATING:
+            return
+        if self.state == State.SHOOTING:
+            self.shoot()
+            return
+    
+    def shoot(self):
+        self.get_logger().info('shooting')
 
 
 def main(args=None):
--- virtuoso_autonomy/virtuoso_autonomy/roboboat/ball_shooter/ball_shooter_states.py
+++ virtuoso_autonomy/virtuoso_autonomy/roboboat/ball_shooter/ball_shooter_states.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
 class State(Enum):
-    START = 1
-    NAVIGATING = 2
-    SHOOTING = 3
-    COMPLETE = 4
+    NAVIGATING = 3
+    SHOOTING = 4
+    COMPLETE = 5
