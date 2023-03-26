9a9d689726364e55a5c4ac0e7199cdd8deab6d27
Manuel Roglan <manueljoseph113@gmail.com>
2023-03-25 23:53:43 -0400

    added shooting

--- virtuoso_autonomy/virtuoso_autonomy/roboboat/semis/semis_node.py
+++ virtuoso_autonomy/virtuoso_autonomy/roboboat/semis/semis_node.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
 from virtuoso_msgs.action import TaskWaypointNav
+from virtuoso_msgs.action import ShootBalls
 from .semis_states import State
 
 class SemisNode(Node):
@@ -24,6 +25,10 @@ class SemisNode(Node):
         self.nav_req = None
         self.nav_result = None
 
+        self.ball_shooter_client = ActionClient(self, ShootBalls, 'shoot_balls')
+        self.ball_shooter_req = None
+        self.ball_shooter_result = None
+
         self.curr_docking_time = 0
 
         self.state = State.START
@@ -44,9 +49,16 @@ class SemisNode(Node):
                 self.start_next_task()
             else:
                 self.curr_docking_time += 1
+        elif self.state == State.BALL_SHOOTING:
+            self.shoot_balls()
     
     def start_next_task(self):
         self.get_logger().info('Starting next task')
+
+        if self.curr_task + 1 == len(self.task_nums):
+            self.state = State.COMPLETE
+            return
+
         self.curr_task += 1
 
         self.state = State.TASK_WAYPOINT_NAVIGATING
@@ -83,6 +95,38 @@ class SemisNode(Node):
             self.state = State.BALL_SHOOTING
         else:
             self.start_next_task()
+    
+    def shoot_balls(self):
+
+        if self.ball_shooter_req is not None:
+            return
+        
+        msg = ShootBalls.Goal()
+
+        self.ball_shooter_req = self.ball_shooter_client.send_goal_async(msg,
+            feedback_callback=self.ball_shooter_feedback_callback)
+        
+        self.ball_shooter_req.add_done_callback(self.ball_shooter_response_callback)
+    
+    def ball_shooter_feedback_callback(self, msg):
+        feedback = msg.feedback
+        self.get_logger().info(f'Feedback: {feedback}')
+    
+    def ball_shooter_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+
+        self.ball_shooter_result = goal_handle.get_result_async()
+        self.ball_shooter_result.add_done_callback(self.ball_shooter_result_callback)
+
+    def ball_shooter_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info(f'Result: {result}')
+        self.start_next_task()
 
 
 def main(args=None):
