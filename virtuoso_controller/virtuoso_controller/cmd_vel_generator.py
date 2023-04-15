6061a19da5fac5fc53600b704f131d8f9157275f
Douglas Chin <33461797+startrek1wars@users.noreply.github.com>
2023-04-15 12:53:34 -0400

    addtl comments

--- virtuoso_controller/virtuoso_controller/cmd_vel_generator.py
+++ virtuoso_controller/virtuoso_controller/cmd_vel_generator.py
@@ -16,6 +16,7 @@ class CmdVelGenerator:
         self.second_waypoint = Pose()
         self.nav2_path = Path()
         self.hold_final_orient = False
+        #This array keeps track of the poses in the path that we've "checked off". 0 means not checked off, 1 means in progress, 2 means checked off.
         self.completedPoses = numpy.zeros(2)
         self.node = None
     
@@ -33,7 +34,7 @@ class CmdVelGenerator:
         dist_to_target = ((self_x - dest_x)**2 + (self_y - dest_y)**2)**(1/2)
         
         min_pose_distance = 100000.0
-        closest_pose = 0
+        closest_pose = 0 #in previous iterations the closest pose was the closest pose to the vehicle. Now it's the "selected" pose, as in we're trying to move along the line segment from this pose to the pose after it in the path
         
         #pick out the next pose
         for i in range(0,len(self.nav2_path.poses)):
@@ -42,11 +43,12 @@ class CmdVelGenerator:
                 closest_pose = i
                 self.completedPoses[i] = 1
                 break
-            if(i == len(self.nav2_path.poses)-1):
+            if(i == len(self.nav2_path.poses)-1): #if all poses have been checked off, select the last pose
                 closest_pose = i
         
         #the path should deliver us to the pose after the "closest_pose". If we are close to that next pose, then we consider the closest pose's segment to be done.
         sec_to_veh = [0.0,0.0,0.0]
+        #have to check if it's the last pose in the path though
         if(closest_pose != len(self.nav2_path.poses) -1):
             sec_to_veh[0] = self_x - self.nav2_path.poses[closest_pose+1].pose.position.x
             sec_to_veh[1] = self_y - self.nav2_path.poses[closest_pose+1].pose.position.y       
@@ -58,12 +60,12 @@ class CmdVelGenerator:
         if(numpy.linalg.norm(sec_to_veh) < 1.0):
             self.completedPoses[closest_pose] = 2
                 
-        #if the closest pose is the last pose, decrement by 1. If this is omitted, the vehicle should just beeline straight for the last pose while ignoring the path.
+        #if the closest pose is the last pose, decrement by 1. If this is omitted, the vehicle might just beeline straight for the last pose while ignoring the path when the last pose is selected.
         if(closest_pose == len(self.nav2_path.poses) - 1 and len(self.nav2_path.poses) > 1):
             closest_pose = closest_pose - 1
         
-        #if the closest pose is in front of us, we want to use the line segment between the closest pose and the pose before it, not the closest pose and the pose after it
-        #if the closest pose is not the current position and it is not the first pose in the path
+        #if the selected ("closest") pose is in front of us, we want to use the line segment between the selected pose and the pose before it, not the selected pose and the pose after it
+        #if the selected pose is not the current position and it is not the first pose in the path
         if((self.nav2_path.poses[closest_pose].pose.position.x != self_x and self.nav2_path.poses[closest_pose].pose.position.y != self_y) and closest_pose != 0):
             #vector from the closest pose to the pose after it
             next_to_second = [0.0,0.0,0.0]
@@ -88,11 +90,11 @@ class CmdVelGenerator:
         
             
             #if the angle is over 90 degrees, we know that we are behind the closest pose so we should decrement in order to use the line segment between the closest pose and the pose in back of it.
-            
+            #if the distance is under 1 meter, we don't decrement, which leads to cutting the corner a bit
             if (abs(nssv_angle) >= numpy.pi/2 and numpy.linalg.norm(next_to_veh) > 1.0):
                 closest_pose = closest_pose - 1
          
-        #We now need to check if we've already passed the current line segment we're on (the segment from the "closest pose" to the pose after it). If so, then we know that portion of the path is no longer relevant to us, so we increment and mark the previous pose's segment as done. We do this until we find a segment that we're either "behind" or "in the middle of"
+        #We now need to check if we've already passed the current line segment we're on ("in front of") (the segment from the "closest pose" to the pose after it). If so, then we know that portion of the path is no longer relevant to us, so we increment and mark the previous pose's segment as done. We do this until we find a segment that we're either "behind" or "in the middle of"
         in_front_of_second_pose = True
         while(in_front_of_second_pose):
             next_to_second = [0.0,0.0,0.0]
@@ -112,6 +114,7 @@ class CmdVelGenerator:
             if (abs(nssv_angle) < numpy.pi/2):
                 self.completedPoses[closest_pose] = 2
                 closest_pose = closest_pose + 1
+                #if it's the last pose in the path, we want to just use that pose, because obviously there aren't any more poses to iterate through
                 if(closest_pose == len(self.nav2_path.poses) - 1):
                     in_front_of_second_pose = False
             else:
@@ -166,12 +169,14 @@ class CmdVelGenerator:
         else:
             sec_to_tar[0] = self.nav2_path.poses[closest_pose].pose.position.x - self.nav2_path.poses[-1].pose.position.x
             sec_to_tar[1] = self.nav2_path.poses[closest_pose].pose.position.y - self.nav2_path.poses[-1].pose.position.y               
-        #slow down if we're getting close to the target
+        #slow down if we're getting close to the target AND the next pose moves us closer to the target
+        #the goal of this second condition is that even though the next pose might be close to the target, it might be moving us away from the target, in which case we don't need to slow down since presumably we're not about to hit the target
         if (dist_to_target < 6.0 and numpy.linalg.norm(sec_to_tar) <= dist_to_target):
             vel_parallel_speed = dist_to_target/2.5
 
             
         #slowdown for turns
+        #Obviously, only need to slow down if it's possible for there to be another line sgement after the current line segment
         if(closest_pose <= len(self.nav2_path.poses) -1 -2):
             next_to_second = [0.0,0.0,0.0]
             next_to_second[0] = self.nav2_path.poses[closest_pose+1].pose.position.x - self.nav2_path.poses[closest_pose].pose.position.x
@@ -184,13 +189,15 @@ class CmdVelGenerator:
             #dot product of the two vectors
             nsst_dot = numpy.dot(next_to_second, sec_to_third)
             
-            #angle between the vector from the closest point to the point after that and the vector between the second point and the vehicle
+            #angle between the vector from the closest point to the point after that and the vector between the second point and the third point
+            #so the angle of turning
             nsst_angle = numpy.arccos(nsst_dot/numpy.linalg.norm(next_to_second)/numpy.linalg.norm(sec_to_third))                
             
             sec_to_veh = [0.0,0.0,0.0]
             sec_to_veh[0] = self_x - self.nav2_path.poses[closest_pose+1].pose.position.x
             sec_to_veh[1] = self_y - self.nav2_path.poses[closest_pose+1].pose.position.y        
             
+            #if the angle of turning is over a certain value and the vehicle is within a certain distance of the turn, then slow down by a factor
             if(abs(nsst_angle) >= numpy.pi/7 and numpy.linalg.norm(sec_to_veh) < 3.0):
                 reduction_factor_turn = min(1.0, nsst_angle*2/numpy.pi)
                 #self.node.get_logger().info(str(reduction_factor_turn))
@@ -209,7 +216,9 @@ class CmdVelGenerator:
         
         vel_parallel_mag = 0.0
         
+        #prevent divide by 0 errors
         if(numpy.linalg.norm(vel_parallel) > 0.0):
+            #find the necessary factor the scale the vector by
             vel_parallel_mag = vel_parallel_speed/((vel_parallel[0])**2 + (vel_parallel[1])**2)**(1/2)
         
         #now actually apply the calculated magnitude of the parallel velocity to its components
@@ -241,7 +250,7 @@ class CmdVelGenerator:
         elif (second_x == next_x and second_y != next_y):
             closest_x = second_x
             closest_y = self_y
-        #if the closest pose and the second closest pose are the same point, then the closest point is just that point
+        #if the closest pose and the second closest pose are the same point, then the closest point on the path is just that point
         else:
             closest_x = next_x
             closest_y = next_y
