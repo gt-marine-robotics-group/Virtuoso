b5290cfe4e39933f9358c90b9cc0c6a182c5102f
Manuel Roglan <manueljoseph113@gmail.com>
2023-04-16 15:09:49 -0400

    publishing cluster viz

--- virtuoso_perception/src/euclidean_clustering_node.cpp
+++ virtuoso_perception/src/euclidean_clustering_node.cpp
@@ -13,6 +13,18 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+
+struct ClusterBounds {
+    float x_min = 0.0;
+    float x_max = 0.0;
+    float y_min = 0.0;
+    float y_max = 0.0;
+    float z_min = 0.0;
+    float z_max = 0.0;
+};
 
 class EuclideanClusteringNode : public rclcpp::Node {
 
@@ -58,6 +70,16 @@ class EuclideanClusteringNode : public rclcpp::Node {
 
         RCLCPP_INFO(this->get_logger(), "EC Indices: %zu", cluster_indices.size());
 
+        visualization_msgs::msg::Marker clear_marker;
+        clear_marker.action = visualization_msgs::msg::Marker::DELETEALL;
+        clear_marker.ns = "perception_ec";
+        visualization_msgs::msg::MarkerArray clear_markers;
+        clear_markers.markers.push_back(clear_marker);
+        m_clusters_viz_pub->publish(clear_markers);
+
+        visualization_msgs::msg::MarkerArray markers;
+        markers.markers.resize(cluster_indices.size());
+
         int j = 1;
         for (const auto& cluster : cluster_indices) {
             pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZ>);
@@ -82,8 +104,70 @@ class EuclideanClusteringNode : public rclcpp::Node {
                 m_cluster2_pub->publish(pub_msg);
             }
 
+            ClusterBounds bounds;
+
+            for (const auto& point : cloud_cluster->points) {
+                if (bounds.x_min == 0.0 || point.x < bounds.x_min) {
+                    bounds.x_min = point.x;
+                } 
+                if (bounds.x_max == 0.0 || point.x > bounds.x_max) {
+                    bounds.x_max = point.x;
+                }
+                if (bounds.y_min == 0.0 || point.y < bounds.y_min) {
+                    bounds.y_min = point.y;
+                }
+                if (bounds.y_max == 0.0 || point.y > bounds.y_max) {
+                    bounds.y_max = point.y;
+                }
+                if (bounds.z_min == 0.0 || point.z < bounds.z_min) {
+                    bounds.z_min = point.z;
+                }
+                if (bounds.z_max == 0.0 || point.z > bounds.z_max) {
+                    bounds.z_max = point.z;
+                }
+            }
+            RCLCPP_INFO(this->get_logger(), "X min: %f", bounds.x_min);
+            RCLCPP_INFO(this->get_logger(), "X max: %f", bounds.x_max);
+
+            visualization_msgs::msg::Marker marker;
+            marker.header.frame_id = "wamv/lidar_wamv_link";
+            marker.id = j;
+            marker.ns = "perception_ec";
+            marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
+            marker.scale.x = 0.5;
+            marker.color.a = 1.0;
+            marker.color.r = 1.0;
+            marker.color.g = 1.0;
+            marker.color.b = 0.0;
+            
+            // Bottom of the cube
+            add_point_to_marker(marker, bounds.x_min, bounds.y_min, bounds.z_min);
+            add_point_to_marker(marker, bounds.x_max, bounds.y_min, bounds.z_min);
+            add_point_to_marker(marker, bounds.x_max, bounds.y_max, bounds.z_min);
+            add_point_to_marker(marker, bounds.x_min, bounds.y_max, bounds.z_min);
+
+            // Top of the cube
+            add_point_to_marker(marker, bounds.x_min, bounds.y_max, bounds.z_max);
+            add_point_to_marker(marker, bounds.x_min, bounds.y_min, bounds.z_max);
+            add_point_to_marker(marker, bounds.x_max, bounds.y_min, bounds.z_max);
+            add_point_to_marker(marker, bounds.x_max, bounds.y_max, bounds.z_max);
+
+            markers.markers[j - 1] = marker;
+
+            RCLCPP_INFO(this->get_logger(), "Marker length: %zu", marker.points.size());
+
             ++j;
         }
+
+        m_clusters_viz_pub->publish(markers);
+    }
+
+    void add_point_to_marker(visualization_msgs::msg::Marker& marker, float x, float y, float z) const {
+        geometry_msgs::msg::Point point;
+        point.x = x;
+        point.y = y;
+        point.z = z;
+        marker.points.push_back(point);
     }
 
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr m_points_sub;
@@ -93,6 +177,8 @@ class EuclideanClusteringNode : public rclcpp::Node {
 
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_cluster1_pub;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_cluster2_pub;
+
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr m_clusters_viz_pub;
 
     public:
         EuclideanClusteringNode() : Node("perception_euclidean_clustering") {
@@ -110,6 +196,9 @@ class EuclideanClusteringNode : public rclcpp::Node {
             );
             m_cluster2_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>(
                 "/cluster2", 10
+            );
+            m_clusters_viz_pub = this->create_publisher<visualization_msgs::msg::MarkerArray>(
+                "/perception/clusters_viz", 10
             );
         }
 };
