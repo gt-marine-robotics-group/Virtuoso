dfbd4f99feee851500878c131a0c745f356f7ba2
Manuel Roglan <manueljoseph113@gmail.com>
2023-12-06 12:25:25 -0500

    untested implementation

--- virtuoso_navigation/virtuoso_navigation/planners/RRT.py
+++ virtuoso_navigation/virtuoso_navigation/planners/RRT.py
@@ -1,4 +1,4 @@
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, PoseStamped
 from nav_msgs.msg import Path
 from virtuoso_navigation.planners.Planner import Planner
 import numpy as np
@@ -6,48 +6,130 @@ import numpy as np
 class RRT(Planner):
 
     def __init__(self):
-        self.extra_after_goal = 10
-        self.extra_before_robot = 3
-
-        self.expand_size = 10
+        self.MAX_ITER_COUNT = 1_000_000
+        self.step_dist = 1
+    
+    def distance(x1, y1, x2, y2):
+        return ((x1 - x2)**2 + (y1 - y2)**2)**0.5
 
     def is_occupied(self, x, y):
 
-        # add origin of self.map to the robot_pose x and y
-        # subtract extra_before_robot
-        # add the x and y positions passed in
+        # Note that x and y are coming in the map frame but we should not index the costmap
+        # with that x and y. It must be transformed to the costmap indices. 
 
-        #nope
-        # should first get four x,y grid bounds based on extra_dist in self.create_path
-        # then the input x and y to those
-        pass
+        x_costmap = abs((self.height_m / 2) - x) / self.map.info.resolution
+        y_costmap = (y + (self.width_m / 2)) / self.map.info.resolution
+
+        x_index = int(x_costmap)
+        y_index = int(y_costmap)
+
+        if self.map.data[(x_index * self.map.info.width) + y_index] > 0:
+            return True
+        
+        return False
 
     def get_random_loc(self):
         
         if np.random.random_sample() < 0.05:
             return (self.goal.position.x, self.goal.position.y)
         
-        x = np.random.random_sample() * self.search_x_dist
-        y = np.random.random_sample() * self.search_y_dist
+        x = np.random.uniform(-(self.height_m / 2), self.height_m / 2)
+        y = np.random.uniform(-(self.width_m / 2), self.width_m / 2)
 
-        # check if occupied
+        while self.is_occupied(x, y):
+            x = np.random.uniform(-(self.height_m / 2), self.height_m / 2)
+            y = np.random.uniform(-(self.width_m / 2), self.width_m / 2)
+        
+        return x, y
 
+    def step_from_to(self, node0, node1):
+
+        if RRT.distance(node0[0], node0[1], node1[0], node1[1]) < self.step_dist:
+            return node1
+        
+        diff = [node1[0] - node0[0], node1[1] - node0[1]]
+
+        diff /= np.sqrt(np.sum(np.square(diff)))
+
+        x = node0[0] + (diff[0] * self.step_dist)
+        y = node0[1] + (diff[1] * self.step_dist)
+
+        return x, y
+    
+    def is_collision_with_obstacles(self, node0, node1):
+        for i in range(1, 5):
+            x = node0[0] + ((node1[0] - node0[0]) * (1/i))
+            y = node0[1] + ((node1[1] - node0[1]) * (1/i))
+
+            if self.is_occupied(x, y):
+                return False
+        return True
 
     def create_path(self, goal: Pose) -> Path:
 
+        self.width_m = self.map.info.width * self.map.info.resolution
+        self.height_m = self.map.info.height * self.map.info.resolution
+
         self.goal = goal
 
-        extra_dist = self.extra_after_goal + self.extra_before_robot
-
-        self.search_x_dist = abs(goal.position.x - self.robot_pose.position.x) + extra_dist
-        self.search_y_dist = abs(goal.position.y - self.robot_pose.position.y) + extra_dist
-
-        self.iteration = 0
-
         self.tree = {}
-        self.nodes = set()
+        self.parents = {}
 
         curr_loc = (self.robot_pose.x, self.robot_pose.y)
 
-        self.nodes.add(curr_loc)
-        self.tree[curr_loc] = {}
+        self.tree[curr_loc] = []
+        self.parents[curr_loc] = 0
+
+        goal_node = None
+
+        iteration = 0
+        while iteration < self.MAX_ITER_COUNT:
+            iteration += 1
+
+            x, y = self.get_random_loc()
+
+            closest_node = None
+            closest_dist = 0
+
+            for node in self.tree.keys():
+                d = RRT.distance(node[0], node[1], x, y)
+                if d < closest_dist:
+                    closest_node = node
+                    closest_dist = d
+            
+            next_node = self.step_from_to(closest_node, (x, y))
+
+            if self.is_collision_with_obstacles(closest_node, next_node):
+                continue
+            
+            self.tree[next_node] = []
+            self.tree[closest_node].append(next_node)
+
+            self.parents[next_node] = closest_node
+
+            if self.distance(next_node[0], next_node[1], self.goal.position.x, self.goal.position.y) < 0.1:
+                goal_node = next_node
+                break
+        
+        if goal_node is None:
+            return Path()
+        
+        path = []
+
+        curr_node = goal_node
+        while curr_node != 0:
+            path.append(curr_node)
+            curr_node = self.parents[curr_node]
+        
+        path = path.reverse()
+
+        # add smoothing sometime
+
+        ros_path = Path()
+        for node in path:
+            p = PoseStamped()
+            p.pose.position.x = node[0]
+            p.pose.position.y = node[1]
+            ros_path.poses.append(p)
+        
+        return ros_path
