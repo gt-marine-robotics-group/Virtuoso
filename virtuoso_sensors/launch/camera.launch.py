8e96b9e0418d8cded6b6e3ecb62568ce71dc8935
Manuel Roglan <59785089+mroglan@users.noreply.github.com>
2024-01-19 01:38:26 -0500

    Oakd integration (#156)
    * Added oakd launch file to sensors main launch file
    
    * Revert "Added oakd launch file to sensors main launch file"
    
    This reverts commit b1a945a3349374e4bc313ec60a8daafce1053831.
    
    * Added oakd to sensors main launch file
    
    * launching oakd with param
    
    ---------
    
    Co-authored-by: santi <sfiz3@gatech.edu>

--- virtuoso_sensors/launch/camera.launch.py
+++ virtuoso_sensors/launch/camera.launch.py
@@ -1,15 +1,19 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition
 from launch_ros.actions import Node
 import sys
+import os
 import yaml
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
     perception_share = get_package_share_directory('virtuoso_perception')
+    oakd_share = get_package_share_directory('depthai_ros_driver')
     
     usv_arg = DeclareLaunchArgument('usv')
     usv_config = LaunchConfiguration('usv')
@@ -23,28 +27,14 @@ def generate_launch_description():
 
     ld.append(usv_arg)
 
-    if usv_config_str == 'robotx':
-        ld.append(
-            Node(
-                package='usb_cam',
-                executable='usb_cam_node_exe',
-            ),
-        )
-        ld.append(
-            Node(
-                package='tf2_ros',
-                executable='static_transform_publisher',
-                name='leftcam_to_baselink',
-                arguments=['1.17', '-0.18', '1.245', '0', '0', '0', 'wamv/base_link', 'wamv/front_left_camera_link_optical'],
-                condition=IfCondition(PythonExpression(["'", usv_config, "' == 'robotx'"]))
-            )
-        )
-    else:
-        with open(f'{perception_share}/config/{usv_config_str}/camera_config.yaml', 'r') \
-            as stream:
-            camera_data = yaml.safe_load(stream)
+    with open(f'{perception_share}/config/{usv_config_str}/camera_config.yaml', 'r') \
+        as stream:
+        camera_data = yaml.safe_load(stream)
 
-        for i, topic in enumerate(camera_data['camera_config']['all_camera_base_topics']):
+    for i, topic in enumerate(camera_data['camera_config']['all_camera_base_topics']):
+        cam_type = camera_data['camera_config']['all_camera_types'][i]
+
+        if cam_type == 'usb':
             ld.append(
                 Node(
                     package='usb_cam',
@@ -71,32 +61,38 @@ def generate_launch_description():
                     ]
                 )
             )
-        
-        for i, frame in enumerate(camera_data['camera_config']['all_camera_frames']):
+        elif cam_type == 'oakd':
             ld.append(
-                Node(
-                    package='tf2_ros',
-                    executable='static_transform_publisher',
-                    name=f'base_link_to_{frame}',
-                    arguments=[
-                        *(str(d) for d in camera_data['camera_config']['all_camera_transforms'][i]),
-                        '0', '0', '0', 'base_link', frame
-                    ]
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(os.path.join(oakd_share, 'launch/rgbd_pcl.launch.py')),
                 )
             )
+    
+    for i, frame in enumerate(camera_data['camera_config']['all_camera_frames']):
+        ld.append(
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                name=f'base_link_to_{frame}',
+                arguments=[
+                    *(str(d) for d in camera_data['camera_config']['all_camera_transforms'][i]),
+                    '0', '0', '0', 'base_link', frame
+                ]
+            )
+        )
 
-        for i, topic in enumerate(camera_data['camera_config']['all_camera_base_topics']):
-            ld.append(
-                Node(
-                    package='virtuoso_sensors',
-                    executable='camera_republish',
-                    name=f'{topic[topic.rfind("/") + 1:]}_republish',
-                    remappings=[
-                        ('input', f'{topic}/image_raw'),
-                        ('output', f'{topic}/image_raw/best_effort')
-                    ]
-                )
+    for i, topic in enumerate(camera_data['camera_config']['all_camera_base_topics']):
+        ld.append(
+            Node(
+                package='virtuoso_sensors',
+                executable='camera_republish',
+                name=f'{topic[topic.rfind("/") + 1:]}_republish',
+                remappings=[
+                    ('input', f'{topic}/image_raw'),
+                    ('output', f'{topic}/image_raw/best_effort')
+                ]
             )
+        )
 
 
     return LaunchDescription(ld)
