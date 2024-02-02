142932f338106ff22aa0947a5ae0d668e7ab8009
Daniel Wu <dwu359@gatech.edu>
2024-02-02 16:25:58 -0600

    a star wip

--- virtuoso_navigation/virtuoso_navigation/planners/AStarPlanner.py
+++ virtuoso_navigation/virtuoso_navigation/planners/AStarPlanner.py
@@ -1,18 +1,23 @@
-# Imports 
+# Imports
+from collections import defaultdict
 import numpy as np
+from typing import Optional, Callable
 from Planner import Planner
 from geometry_msgs.msg import Pose
 from nav_msgs.msg import OccupancyGrid, Path
+import heapq
 
 import math
 from enum import Enum
 
 MAX_INT32 = np.iinfo(np.int32).max
 
+
 class SquareTypes(Enum):
     """
     Holds the values that will be used while a_star calculates the optimal path
     """
+
     EMPTY_SQUARE = 0
     OBSTACLE = -1
     OPEN_SET = 1
@@ -20,23 +25,26 @@ class SquareTypes(Enum):
 
 
 class AStarPlanner(Planner):
-    
     def __init__(self, inflation_layer):
         super().__init__(inflation_layer)
-        
-    def create_path(self, goal: Pose) -> Path:
-        start_pos: tuple[int, int] = self.position_to_node(self.robot_pose.position.x, self.robot_pose.position.y)
-        end_pos: tuple[int, int] = self.position_to_node(goal.position.x, goal.position.y)
 
-        map_array: np.array = self.occupancy_grid_to_nparray(self.map)
-        
-        grid_path: list[tuple[int, int]] = self.a_star(map_array, start_pos, end_pos)
-        
-        return self.construct_path(grid_path)     
-        
+    def create_path(self, goal: Pose) -> Path:
+        start_pos: tuple[int, int] = self.map_pos(
+            self.robot_pose.position.x, self.robot_pose.position.y
+        )
+        end_pos: tuple[int, int] = self.map_pos(goal.position.x, goal.position.y)
+        width = self.map.info.width
+        height = self.map.info.height
+        grid = np.reshape(np.array(self.map.data), (height, width))
+        grid_path: list[tuple[int, int]] = self.a_star(grid, start_pos, end_pos)
+
+        return self.construct_path(grid_path)
+
     # Heutistic functions for a_star
     @staticmethod
-    def euclidian_dist(start_point: tuple[int, int], end_point: tuple[int, int]) -> float:
+    def euclidian_dist(
+        start_point: tuple[int, int], end_point: tuple[int, int]
+    ) -> float:
         """
         Gives the normal 2d Euclidian distance between 2 points
         """
@@ -44,49 +52,48 @@ class AStarPlanner(Planner):
         a = end_point[0] - start_point[0]
         b = end_point[1] - start_point[1]
 
-        return np.sqrt(a*a + b*b) 
+        return np.sqrt(a * a + b * b)
 
-    def occupancy_grid_to_nparray(occupancy_grid: OccupancyGrid):
-        """
-        Convert a ROS2 OccupancyGrid message to a numpy array.
-
-        Args:
-        occupancy_grid (nav_msgs.msg.OccupancyGrid): The OccupancyGrid message.
-
-        Returns:
-        numpy.ndarray: A 2D numpy array representing the occupancy grid.
-        """
-        # Get the width and height from the OccupancyGrid
-        width = occupancy_grid.info.width
-        height = occupancy_grid.info.height
-
-        # Convert the data to a numpy array
-        data = np.array(occupancy_grid.data)
-
-        # Reshape the numpy array to 2D based on width and height of the grid
-        return np.reshape(data, (height, width))
- 
     # Util functions for other nodes to interface with
-    def position_to_node(occupancy_grid: OccupancyGrid, pos_x: float, pos_y: float):
-        grid_x = math.floor(pos_x / occupancy_grid.resolution)
-        grid_y = math.floor(pos_y / occupancy_grid.resolution)
+    def map_pos(self, pos_x: Optional[float] = 0, pos_y: Optional[float] = 0):
+        grid_x, grid_y = 0, 0
+        if self.map.info.resolution is float:
+            grid_x = math.floor(pos_x / self.map.info.resolution)
+            grid_y = math.floor(pos_y / self.map.info.resolution)
 
-        grid_x = occupancy_grid.width // 2 + grid_x
-        grid_y = occupancy_grid.height // 2 - grid_y
+        grid_x = self.map.info.width // 2 + grid_x
+        grid_y = self.map.info.height // 2 - grid_y
 
-        if (grid_x > occupancy_grid.width or grid_x < 0) or (grid_y > occupancy_grid.height or grid_y < 0):
-            return None
-        
+        if (grid_x > self.map.info.width or grid_x < 0) or (
+            grid_y > self.map.info.height or grid_y < 0
+        ):
+            raise Exception("invalid grid_x or grid_y")
+
         return (grid_x, grid_y)
 
-    def a_star(self, map_array: np.array, start_pos: tuple[int, int],
-               end_pos: tuple[int, int],
-               h_func: callable = euclidian_dist) -> list[tuple[int, int]]:
+    def a_star(
+        self,
+        map_array: np.ndarray,
+        start_pos: tuple[int, int],
+        end_pos: tuple[int, int],
+        h_func: Callable = euclidian_dist,
+    ) -> list[tuple[int, int]]:
+        came_frm = dict()
+        g_scor = defaultdict(lambda: float("inf"))
+        g_scor[start_pos] = 0
+        f_scor = defaultdict(lambda: float("inf"))
+        f_scor[start_pos] = h_func(start_pos)
+        open_heap = [(f_scor, start_pos)]
 
-        open_close_set: np.array = np.full(map_array.shape, np.NaN)
+        while open_heap:
+            f, pos = heapq.heappop(open_heap)
+            if pos == end_pos:
+                return 0
+
+        open_close_set: np.ndarray = np.full(map_array.shape, np.NaN)
         open_close_set[start_pos] = SquareTypes.OPEN_SET
 
-        came_from: np.array = np.full(map_array.shape, np.NaN, dtype=np.int32)
+        came_from: np.ndarray = np.full(map_array.shape, np.NaN, dtype=np.int32)
 
         g_score = np.full(map_array.shape, MAX_INT32, dtype=np.int32)
         g_score[start_pos] = 0
@@ -95,51 +102,58 @@ class AStarPlanner(Planner):
         f_score[start_pos] = h_func(start_pos, end_pos)
 
         while np.any(open_close_set == SquareTypes.OPEN_SET):
-            f_mask = f_score.copy();
+            f_mask = f_score.copy()
             f_mask[open_close_set != 1] = np.Inf
-            
+
             current = np.unravel_index(np.argmin(f_mask), map_array)
-            
+
             if current == end_pos:
-                return self.reconstruct_gridpath(came_from, start_pos, end_pos, map_array.shape)
-            
+                return self.reconstruct_gridpath(
+                    came_from, start_pos, end_pos, map_array.shape
+                )
+
             open_close_set[current] = 2
-            
+
             for i in range(-1, 2):
                 for j in range(-1, 2):
                     neighbor = (current[0] + i, current[1] + j)
-                    if (i == 0 and j == 0) or not self.is_valid_neighbor(map_array, neighbor):
+                    if (i == 0 and j == 0) or not self.is_valid_neighbor(
+                        map_array, neighbor
+                    ):
                         continue
-                    
+
                     grid_dist = 1 if (i == 0 or j == 0) else np.sqrt(2)
-                    
+
                     temp_g = g_score[current] + grid_dist
-                    
-                    
+
                     if temp_g < g_score[neighbor]:
-                        came_from[neighbor] = np.ravel_multi_index(current, map_array.shape)
-                        
+                        came_from[neighbor] = np.ravel_multi_index(
+                            current, map_array.shape
+                        )
+
                         g_score[neighbor] = temp_g
                         f_score[neighbor] = temp_g + h_func(neighbor, end_pos)
-                        
+
                         open_close_set[neighbor] = 1
-                        
+
         return None
-    
+
     def is_valid_neighbor(map_array: np.array, neighbor: tuple[int, int]):
         for i in range(len(neighbor)):
             if neighbor[i] < 0 or neighbor[i] >= map_array.shape[i]:
                 return False
-            
+
         if map_array[neighbor] == SquareTypes.OBSTACLE:
             return False
-        
-        return True
-    
 
-    def reconstruct_gridpath(came_from: np.array, start_pos: tuple[int, int], 
-                         end_pos: tuple[int, int], grid_shape: tuple[int, int]) -> list[tuple[int, int]]:
-        
+        return True
+
+    def reconstruct_gridpath(
+        came_from: np.array,
+        start_pos: tuple[int, int],
+        end_pos: tuple[int, int],
+        grid_shape: tuple[int, int],
+    ) -> list[tuple[int, int]]:
         current = end_pos
         path = [current]
 
--- virtuoso_navigation/virtuoso_navigation/planners/Planner.py
+++ virtuoso_navigation/virtuoso_navigation/planners/Planner.py
@@ -3,27 +3,26 @@ from nav_msgs.msg import OccupancyGrid, Path
 from geometry_msgs.msg import Pose
 from virtuoso_mapping.inflation_layer import InflationLayer
 
+
 class Planner(ABC):
-
     def __init__(self, inflation_layer):
-
         self.map: OccupancyGrid = None
         self.robot_pose: Pose = None
 
         self._inflation_layer = InflationLayer(inflation_layer)
 
         self.node = None
-    
+
     def debug(self, msg: str):
         if self.node is None:
             return
         self.node.get_logger().info(msg)
-    
+
     @abstractmethod
     def create_path(self, goal: Pose) -> Path:
         pass
 
-    def pose_deep_copy(pose:Pose):
+    def pose_deep_copy(self, pose: Pose):
         copy = Pose()
         copy.position.x = pose.position.x
         copy.position.y = pose.position.y
